89fe5850d27047720f33382c7bd653f129080179
ula_s <ula@brightinventions.pl>
2020-06-25 11:29:41 +0200

    Delete "the"

--- src/components/home/Cocoaheads.tsx
+++ src/components/home/Cocoaheads.tsx
@@ -8,7 +8,7 @@ const Cocoaheads: FC<CocoaheadsProps> = props => {
   return (
     <Section className="has-background-black has-text-centered">
       <SectionTitle className="is-size-3 has-text-white">
-        we are a co-host of the Cocoaheads Tricity
+        we are a co-host of Cocoaheads Tricity
       </SectionTitle>
       <ButtonInverted>when is next meeting?</ButtonInverted>
     </Section>
