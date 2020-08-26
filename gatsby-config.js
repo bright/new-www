82372e17d9c906248634f3c3caaaaa885bd6b1ae
Paweł Papkiewicz <pawel.papkiewicz@brightinventions.pl>
2020-08-26 09:20:17 +0200

    Add netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,7 +83,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
-
+    `gatsby-plugin-netlify-cms`,
     `gatsby-transformer-remark`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
