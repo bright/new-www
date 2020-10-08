c42b109bf29510fcce8250940f6e30cb4d0242fb
Krzysiek Hinc <krzysztof.hinc@brightinventions.pl>
2020-10-08 09:56:23 +0200

    import reformat

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,7 +1,7 @@
 import { graphql } from 'gatsby'
 import React from 'react'
 
-import Layout from "../components/layout"
+import Layout from '../components/layout'
 import HelmetWrapper from '../components/subcomponents/HelmetWrapper'
 import ProjectCard, {ProjectGraphql} from '../components/subcomponents/ProjectCard'
 
