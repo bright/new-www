a4da30876f4508739ae648e16929853436c1642f
Krzysiek Hinc <krzysztof.hinc@brightinventions.pl>
2020-10-08 09:49:54 +0200

    filter projects with 'published' field

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,38 +1,38 @@
-import { graphql } from "gatsby"
-import React from "react"
+import { graphql } from 'gatsby'
+import React from 'react'
+
 import Layout from "../components/layout"
-import HelmetWrapper from "../components/subcomponents/HelmetWrapper"
-import ProjectCard, {
-  ProjectGraphql,
-} from "../components/subcomponents/ProjectCard"
+import HelmetWrapper from '../components/subcomponents/HelmetWrapper'
+import ProjectCard, {ProjectGraphql} from '../components/subcomponents/ProjectCard'
 
 const ProjectsPage = ({
   data: {
     allMarkdownRemark: { edges },
   },
 }) => {
+  const projects: ProjectGraphql[] = edges
+      .map(({ node: { frontmatter } }: {node: {frontmatter: ProjectGraphql}}) => frontmatter)
+      .filter((project: ProjectGraphql) => project.published)
+
   return (
     <Layout>
       <HelmetWrapper
-        title="Projects"
-        description="About our software development projects"
+        title='Projects'
+        description='About our software development projects'
       />
-      <div className="container">
-        <div className="section">
-          <h1 className="title has-text-dark has-text-weight-bold">
-            discover our projects
-          </h1>
-          <div className="content">
+      <div className='container'>
+        <div className='section'>
+          <div className='content'>
             Since 2012 we have realized many innovative projects among which
             there are solutions supporting eco-driving, application for
             sportsmen, POS cash register, system supporting answering calls to
             emergency numbers and many others.
           </div>
           <div>
-            <div className="columns is-multiline">
-              {edges.map(({ node: { frontmatter } }, index) => (
-                <div className="column is-one-third" key={index + "project"}>
-                  <ProjectCard project={frontmatter as ProjectGraphql} />
+            <div className='columns is-multiline'>
+              {projects.map((project) => (
+                <div className='column is-one-third' key={project.title}>
+                    <ProjectCard project={project} />
                 </div>
               ))}
             </div>
@@ -52,8 +52,8 @@ export const pageQuery = graphql`
             title
             image
             layout
-            slug
             published
+            tags
           }
         }
       }
