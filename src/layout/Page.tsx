b99c8519001af9efbfa97312927fea8e7ac428fd
Krzysiek Hinc <krzysztof.hinc@brightinventions.pl>
2020-10-23 14:31:11 +0200

    page component

--- src/layout/Page.tsx
+++ src/layout/Page.tsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import classNames from 'classnames'
 
 import {Footer} from './Footer'
 import HelmetWrapper from '../components/subcomponents/HelmetWrapper'
 import {TopNavigation} from '../components/subcomponents/TopNavigation'
 
+import styles from './Page.module.scss'
 import '../styles/main.scss'
 
-export const Page: React.FC<{ className?: string }> = ({ children, className }) => {
-  return (
-    <div className={"layout-container " + className}>
-      <HelmetWrapper
-        title="Software Development Company"
-        description="The best custom software development company in Poland. Through mobile apps and complex backend systems to emerging technology solutions we are creating success stories for startups, consultancy agencies as well as mid-size organisations across multiple industries including FinTech, Blockchain, HealthTech, Retail, Logistics and more."
-      />
-      <TopNavigation />
-      {children}
-      <Footer />
-    </div>
-  )
+export const Page: React.FC<{ className?: string }> = ({children, className}) => {
+    const [currentPath, setCurrentPath] = useState<string>()
+    const [mobileMenuOpened, setMobileMenuOpened] = useState(false)
+
+    useEffect(() => {
+        setCurrentPath(window.location.pathname)
+    }, [window.location.pathname])
+
+    return (
+        <div className={classNames('layout-container', className, {[styles.menuOpened]: mobileMenuOpened})}>
+            <HelmetWrapper
+                title="Software Development Company"
+                description="The best custom software development company in Poland. Through mobile apps and complex backend systems to emerging technology solutions we are creating success stories for startups, consultancy agencies as well as mid-size organisations across multiple industries including FinTech, Blockchain, HealthTech, Retail, Logistics and more."
+            />
+            <TopNavigation path={currentPath} toggled={setMobileMenuOpened} />
+            {children}
+            <Footer/>
+        </div>
+    )
 }
