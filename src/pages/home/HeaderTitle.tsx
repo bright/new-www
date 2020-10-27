1d3dc3df1804433b07c163e324318ba6e20e5429
Krzysiek Hinc <krzysztof.hinc@brightinventions.pl>
2020-10-27 17:17:00 +0100

    header to style modules

---
+++ src/pages/home/HeaderTitle.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import classNames from 'classnames'
+
+import styles from './Header.module.scss'
+
+const HeaderTitle = () => {
+  return (
+    <React.Fragment>
+      <div className='column'>
+        <h1 className={classNames('title mt-6', styles.title)}>
+          let's create software that <span>matters</span>
+        </h1>
+      </div>
+      <div className='column is-hidden-tablet'>
+        Through mobile apps and complex backend systems to emerging
+        technology solutions we are creating success stories for startups,
+        consultancy agencies as well as mid-size organisations
+      </div>
+      <div className='column is-hidden-tablet'>
+        <div className='buttons'>
+          <a className='button estimate is-primary' href='/start-project'>
+            <strong>let's talk</strong>
+          </a>
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default HeaderTitle
