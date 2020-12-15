63d390dcf19c7902e62cf5f3b13c7f3a573b05cb
Bartosz Szafran <bartosz.szafran@practi.net>
2020-12-15 21:45:48 +0100

    Add showcase carousel

--- src/components/whatWeDo/showcase/ShowcaseCarousel.tsx
+++ src/components/whatWeDo/showcase/ShowcaseCarousel.tsx
@@ -1,63 +1,109 @@
+import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
+import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import styled from "styled-components"
+import BackArrow from "../../../assets/backArrowBlack.svg"
+import NextArrow from "../../../assets/nextArrowBlack.svg"
+import { ProjectModel } from "../../../models/gql"
+import variables from "../../../styles/variables"
+import CarouselCard from "./CarouselCard"
 
-import React from 'react'
-import classNames from 'classnames'
-import {Carousel} from 'react-responsive-carousel'
+const Indicator = styled.li<{ selected?: boolean }>({
+  color: variables.black,
+  fontSize: "28px",
+  display: "inline-block",
+  marginTop: "20px",
+  position: "relative",
+})
 
-import ProjectCard from '../../components/subcomponents/ProjectCard'
-import {ProjectModel} from '../../models/gql'
+const CarouselWrapper = styled.div({
+  marginBottom: "105px",
 
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
-import BackArrow from '../../assets/backArrowBlack.svg'
-import NextArrow from '../../assets/nextArrowBlack.svg'
-import styles from './Header.module.scss'
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
 
+  ".carousel": {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
 
-interface Props {
-    items: ProjectModel[]
-}
+    maxWidth: "1730px",
+  },
+})
 
-const ShowcaseCarousel: React.FC<Props> = ({items}) => {
-    // @todo: to fix TS type
-    const indicator = (onClickHandler: any, isSelected: boolean) => {
-        if (isSelected) {
-            return <li className={classNames(styles.indicator, styles.active)}>&#x2022;</li>
-        }
-        return (
-            <li className={styles.indicator} onClick={onClickHandler}>
-                &#x2022;
-            </li>
-        )
-    }
+const ShowcaseCarousel = () => {
+  const {
+    allMarkdownRemark: { edges },
+  } = useStaticQuery(GQL)
+  const carouselItems: ProjectModel[] = edges
+    ? edges.map((e: any) => e.node.frontmatter)
+    : []
 
-    const arrowPrev = (onClickHandler: any) => (
-        <div className={styles.arrow} style={{left: 15}}>
-            <BackArrow onClick={onClickHandler}/>
-        </div>
-    )
-
-    const arrowNext = (onClickHandler: any) => (
-        <div className={styles.arrow} style={{right: 15}}>
-            <NextArrow onClick={onClickHandler}/>
-        </div>
-    )
-
+  const indicator = (onClickHandler: any, isSelected: boolean) => {
     return (
-        <Carousel
-            className={styles.carousel}
-            showStatus={false}
-            showThumbs={false}
-            infiniteLoop
-            renderIndicator={indicator}
-            renderArrowPrev={arrowPrev}
-            renderArrowNext={arrowNext}>
-            {(items || []).map((item, index) => (
-                <div key={index + 'project'}>
-                    <ProjectCard project={item as ProjectModel} invertTitle={true}/>
-                </div>
-            ))}
-        </Carousel>
+      <Indicator selected={isSelected} onClick={!isSelected && onClickHandler}>
+        &#x2022;
+      </Indicator>
     )
+  }
+
+  const arrowPrev = (onClickHandler: any) => (
+    <div>
+      <BackArrow onClick={onClickHandler} />
+    </div>
+  )
+
+  const arrowNext = (onClickHandler: any) => (
+    <div>
+      <NextArrow onClick={onClickHandler} />
+    </div>
+  )
+
+  return (
+    <CarouselWrapper>
+      <Carousel
+        showStatus={false}
+        showThumbs={false}
+        infiniteLoop
+        renderIndicator={indicator}
+        renderArrowPrev={arrowPrev}
+        renderArrowNext={arrowNext}
+      >
+        {(carouselItems || []).map(item => (
+          <div key={item.title}>
+            <CarouselCard project={item as ProjectModel} />
+          </div>
+        ))}
+      </Carousel>
+    </CarouselWrapper>
+  )
 }
 
 export default ShowcaseCarousel
+
+const GQL = graphql`
+  {
+    allMarkdownRemark(
+      filter: {
+        frontmatter: { layout: { eq: "project" }, published: { ne: false } }
+      }
+      limit: 6
+      sort: { order: ASC, fields: frontmatter___order }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            image
+            layout
+            slug
+            published
+            description
+          }
+        }
+      }
+    }
+  }
+`
