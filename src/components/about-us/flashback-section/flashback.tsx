10819c1d579e90d16c998e8b7eab077e666ed0c9
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2021-05-02 15:12:37 +0200

    Use StaticImages for flashback.tsx

--- src/components/about-us/flashback-section/flashback.tsx
+++ src/components/about-us/flashback-section/flashback.tsx
@@ -1,34 +1,71 @@
+import React from 'react'
 import {
   Button,
-  FlashbackContainer,
-} from "../about-us.styled"
-import React from "react"
-import { images_landscape, images_portrait } from "./flashback-data"
+  FlashbackContainer
+} from '../about-us.styled'
+import { StaticImage } from 'gatsby-plugin-image'
+
+export const images_portrait = [
+  <StaticImage src='../../../../static/images/why-us/history-flashbacks/portrait/one.png'
+               alt='Group of people taking part in a workshop' />
+  ,
+  <StaticImage src='../../../../static/images/why-us/history-flashbacks/portrait/two.png'
+               alt='Software developer at work' />
+  ,
+  <StaticImage src='../../../../static/images/why-us/history-flashbacks/portrait/three.png'
+               alt='Mobile App Developer at work' />
+  ,
+  <StaticImage src='../../../../static/images/why-us/history-flashbacks/portrait/four.png'
+               alt='HR at work' />
+  ,
+  <StaticImage src='../../../../static/images/why-us/history-flashbacks/portrait/five.png'
+               alt='Software developers collaborating' />
+]
+
+export const images_landscape = [
+  <StaticImage src='../../../../static/images/why-us/history-flashbacks/landscape/one.png'
+               alt='Group of people taking part in a workshop' />
+  ,
+  <StaticImage src='../../../../static/images/why-us/history-flashbacks/landscape/two.png'
+               alt='Software developer at work' />
+  ,
+  <StaticImage src='../../../../static/images/why-us/history-flashbacks/landscape/three.png'
+               alt='Mobile App Developer at work' />
+  ,
+  <StaticImage src='../../../../static/images/why-us/history-flashbacks/landscape/four.png'
+               alt='HR at work' />
+  ,
+  <StaticImage src='../../../../static/images/why-us/history-flashbacks/landscape/five.png'
+               alt='Software developers collaborating' />
+  ,
+  <StaticImage src='../../../../static/images/why-us/history-flashbacks/landscape/six.png'
+               alt='Event storming session' />
+]
 
 export function FlashbackComponent() {
   return (
     <FlashbackContainer
-      className="tab-content content"
+      className='tab-content content'
       length={images_portrait.length}
     >
       <h2>history flashbacks</h2>
-      <div className="images images--portrait">
+      <div className='images images--portrait'>
         {images_portrait.map(image => (
-          <img src={image.src} key={image.src} alt={image.alt} />
+          { ...image }
         ))}
       </div>
-      <div className="images images--landscape">
+      <div className='images images--landscape'>
         {images_landscape.map(image => (
-          <img src={image.src} key={image.src} alt={image.alt} />
+          { ...image }
         ))}
       </div>
       <div>
         <a
-          href="https://www.instagram.com/bright_inventions/"
-          target="_blank"
-          rel="noopener noreferrer nofollow"
+          href='https://www.instagram.com/bright_inventions/'
+          target='_blank'
+          rel='noopener noreferrer nofollow'
         >
-          <Button role="link">visit instagram</Button>
+          <Button role='link'>visit instagram</Button>
         </a>
       </div>
     </FlashbackContainer>
