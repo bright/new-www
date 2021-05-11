5c2f53fab9c542896e39c665dd5e202e2efa2006
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2021-05-11 22:18:10 +0200

    #275 Use root folder for sitemap so that the sitemap-index file refers to proper location of sitemap-0.xml

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,7 +114,12 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
-    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: '/'
+      }
+    },
     {
       resolve: 'gatsby-plugin-s3',
       options: {
