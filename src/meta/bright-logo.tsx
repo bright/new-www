7df4e6eba58c21cee5e7e402f742ca4ae753b732
Izabela <82568642+izabelapawlik@users.noreply.github.com>
2021-05-26 12:12:48 +0200

    Expose new image
    A new image for share in social media #294

--- src/meta/bright-logo.tsx
+++ src/meta/bright-logo.tsx
@@ -5,6 +5,9 @@ import React from 'react'
 import b_logo from '../../static/images/b_logo_black.png'
 // @ts-ignore
 import fb_b_logo from '../../static/images/fb_b_logo_black.png'
+// @ts-ignore
+import fb_share_image from '../../static/images/fb_graph_meta-tag.png'
 
 export const brightLogoShortBlack = b_logo
 export const fbCompatibleBrightLogoShortBlack = fb_b_logo
+export const fbShareImage = fb_share_image
