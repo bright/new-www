3a0c974576cfbf5c3e117556628ec7f3e278db4d
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2021-06-07 16:22:45 +0200

    Add missing fix-cannonical.ts script

---
+++ fix-cannonical.ts
@@ -0,0 +1,53 @@
+import { readdir, readFile, writeFile } from 'fs/promises'
+import { join as pathJoin } from 'path'
+import { promisify } from 'util'
+import readline from 'readline'
+import matter from 'gray-matter'
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+async function answer(question: string): Promise<string> {
+  return new Promise(resolve => {
+    rl.question(question, resolve)
+  })
+}
+
+async function main() {
+  const basePath = pathJoin(__dirname, 'src', 'mdData', 'blog')
+  const blogPostsFiles = await readdir(basePath)
+
+  const blogPosts = await Promise.all(
+    blogPostsFiles.map(async blogPost => {
+      const filePath = pathJoin(basePath, blogPost)
+      return {
+        matter: matter(await readFile(filePath)),
+        filePath,
+      }
+    })
+  )
+
+  const blogPostsCross = blogPosts
+    .filter(post => post.matter.data.crosspost)
+    .filter(post => post.matter.data.author == 'piotr')
+    .filter(post => !post.matter.data.canonical_url)
+
+  for (const blogPost of blogPostsCross) {
+    const canonicalUrl = await answer(`Canonical url for ${blogPost.filePath}:\n`)
+    if (canonicalUrl) {
+      console.log('Setting canonical url to', canonicalUrl)
+      blogPost.matter.data.canonical_url = canonicalUrl
+      const newContent = matter.stringify(blogPost.matter, blogPost.matter.data)
+      await writeFile(blogPost.filePath, newContent)
+    }
+  }
+
+  rl.close()
+}
+
+main().catch(err => {
+  console.error(err)
+  process.exit(1)
+})
