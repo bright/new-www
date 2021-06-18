18448c8fd900f185dabc2cc2d5b2f2c93a8191f0
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2021-06-18 15:33:35 +0200

    Remove unecessary log

--- src/templates/AboutUsTemplate.tsx
+++ src/templates/AboutUsTemplate.tsx
@@ -21,7 +21,6 @@ export default function Template({
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
 
-  console.log('Setting data', data)
   const avatarImage = getImage(frontmatter.avatar)!
   return (
     <Page>
