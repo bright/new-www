2fa0113f46fcec05c52b3092e7aa7acedaff6804
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2021-06-25 10:18:55 +0200

    Domyślnie kieruj na /about-us/story/
    Fixes #261

--- src/config/routing.ts
+++ src/config/routing.ts
@@ -1,7 +1,7 @@
 export const routeLinks = {
   aboutUs(params?: { authorId: string, slug: string | undefined } | { page: 'story' | 'values' | 'team' }) {
     if (!params) {
-      return `/about-us/`
+      return `/about-us/story/`
     }
     if ('authorId' in params) {
       return `/about-us/${params.slug ?? params.authorId}/`
