8ccb7b9a016cc78a39a46ff235a8f12d335225cc
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2021-07-27 16:28:11 +0200

    Make blog post preview look like on target blog. Allow editing tags with spaces.
    Fixes #452

---
+++ src/cms/blog.tsx
@@ -0,0 +1,84 @@
+import { CmsWidgetControlProps, CmsWidgetPreviewProps } from 'netlify-cms-core'
+import { PostTags } from '../PostTags'
+import { Tag, WithContext as ReactTags } from 'react-tag-input'
+import styled from 'styled-components'
+
+const ReactTagsContainer = styled.div`
+  // mostly copy paste from bulma tags
+  // can't get it to work in a clean way
+  .tags {
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    display: flex;
+    align-items: center;
+  }
+
+  .tag {
+    align-items: center;
+    border-radius: 2px;
+    display: inline-flex;
+    height: 2em;
+    justify-content: center;
+    line-height: 1.5;
+    padding-left: 0.75em;
+    padding-right: 0.75em;
+    white-space: nowrap;
+
+    button {
+      margin-left: 0.5em;
+    }
+  }
+`
+
+function toTag(value: string, index: number): Tag {
+  return {
+    id: index.toString(),
+    text: value,
+  }
+}
+
+function toTagValue(tag: Tag) {
+  return tag.text
+}
+
+export const TagsControl: React.FC<CmsWidgetControlProps> = props => {
+  const tags = Array.from(props.value).map((val, ix) => toTag(val as string, ix))
+
+  function onAdd(newTag: Tag) {
+    const updatedTags = tags.concat([newTag])
+    props.onChange(updatedTags.map(toTagValue))
+  }
+
+  function onDelete(index: number) {
+    const updatedTags = tags.filter((_, ix) => ix != index)
+    props.onChange(updatedTags.map(toTagValue))
+  }
+
+  function onDrag(tag: Tag, oldIx: number, newPos: number) {
+    const updatedTags = tags.concat([])
+    updatedTags[oldIx] = updatedTags[newPos]
+    updatedTags[newPos] = tag
+    props.onChange(updatedTags.map(toTagValue))
+  }
+
+  return (
+    <>
+      <ReactTagsContainer>
+        <ReactTags
+          tags={tags}
+          handleAddition={onAdd}
+          handleDelete={onDelete}
+          handleDrag={onDrag}
+          classNames={{
+            tag: 'tag',
+            tags: 'tags',
+          }}
+        />
+      </ReactTagsContainer>
+    </>
+  )
+}
+export const TagsPreview: React.FC<CmsWidgetPreviewProps> = props => {
+  const elements: string[] = props.value
+  return <PostTags tags={elements} />
+}
--- src/cms/cms.tsx
+++ src/cms/cms.tsx
@@ -1,34 +1,10 @@
-import type { CmsWidgetControlProps, CmsWidgetPreviewProps } from 'netlify-cms-core'
 import CMS from 'netlify-cms-app'
-import { PostTags } from '../PostTags'
-import '../styles/main.scss'
+
+// this only seems to register styles in preview pane
+// or nowhere at all
+// import '../styles/main.scss'
 import { BlogPostPreview } from './BlogPostPreview'
+import { TagsControl, TagsPreview } from './blog'
 
-const ArrayControl: React.FC<CmsWidgetControlProps> = props => {
-  const separator = props.field.get('separator', ', ')
-
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    props.onChange(e.target.value.split(separator))
-  }
-
-  return (
-    <input
-      type={'text'}
-      id={props.forID}
-      value={props.value ? props.value.join(separator) : ''}
-      onChange={e => handleChange(e)}
-      className={props.classNameWrapper}
-    />
-  )
-}
-
-const ArrayPreview: React.FC<CmsWidgetPreviewProps> = props => {
-  const elements: string[] = props.value
-  return <PostTags tags={elements} />
-}
-
-// @ts-ignore
-// TODO: configure access to global CMS object
-CMS.registerWidget('array', ArrayControl, ArrayPreview)
-
+CMS.registerWidget('tags', TagsControl, TagsPreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
