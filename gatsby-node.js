a14340a69dfc8786cd983d3c55ffe6213ee7266f
codersbros <p.grden@codersbros.pl>
2021-08-19 19:08:12 +0200

    test deploy

--- gatsby-node.js
+++ gatsby-node.js
@@ -133,15 +133,4 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
-    resolve: {
-      alias: {
-        path: require.resolve("path-browserify")
-      },
-      fallback: {
-        fs: false
-      }
-    }
-  });
-};
+
