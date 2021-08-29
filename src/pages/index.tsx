aa1c56f1cc436776645df42878f38db3a518d819
codersbros <p.grden@codersbros.pl>
2021-08-29 17:14:36 +0200

    remove carousel and add blockchain experts section to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,28 @@
 import React from 'react'
 
-import {Page} from '../layout/Page'
-import {Header} from '../components/home/Header'
-import {Achievements} from '../components/home/Achievements'
-import {Carousel} from '../components/shared/Carousel'
-import {TechnologyTags} from '../components/shared/TechnologyTags'
-import {OurServices} from '../components/home/OurServices'
-import {ImageSpacer} from '../components/shared/ImageSpacer'
-import {Projects} from '../components/home/Projects'
+import { Page } from '../layout/Page'
+import { Header } from '../components/home/Header'
+import { Achievements } from '../components/home/Achievements'
+import { TechnologyTags } from '../components/shared/TechnologyTags'
+import { OurServices } from '../components/home/OurServices'
+import { Projects } from '../components/home/Projects'
 import Ratings from '../components/shared/Ratings'
-import {Contact} from '../components/shared/Contact'
-import { HideTablet } from '../components/shared'
-
+import { Contact } from '../components/shared/Contact'
+import HeroHeaderImages from '../components/home/HeroHeaderImages'
 import { PopularBlogPosts } from '../components/home/PopularBlogPosts'
-
 import '../styles/_page-index.scss'
+import { BlockchainExperts } from '../components/home/BlockchainExperts'
 export default () => {
   return (
-    <Page className="page-index">
+    <Page className='page-index'>
       <Header />
       <Achievements />
-      <HideTablet>
-        <Carousel />
-      </HideTablet>
+      <HeroHeaderImages />
       <TechnologyTags />
       <OurServices />
-      <ImageSpacer />
-
+      <BlockchainExperts />
       <Projects />
       <Ratings />
-
       <PopularBlogPosts />
       <Contact />
     </Page>
