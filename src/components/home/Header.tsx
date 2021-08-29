c3066018f08efe2d4f376c7ebe8db5f3dcd5660a
codersbros <p.grden@codersbros.pl>
2021-08-29 17:22:46 +0200

    fit header title

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -3,26 +3,40 @@ import classNames from 'classnames'
 
 import { routeLinks } from '../../config/routing'
 import { Section } from '../shared'
-
+import styled from 'styled-components'
 import * as styles from './Header.module.scss'
+import variables from '../../styles/variables'
+
+const HeroWrapper = styled.div`
+  max-width: 1449px;
+  @media ${variables.device.mobile} {
+    max-width: 347px;
+  }
+`
+const HeroBody = styled.div`
+  padding: 0 19rem 0 6.5rem;
+  @media ${variables.device.mobile} {
+    padding: 8.94rem 2.56rem 0 0.625rem;
+  }
+`
 
 export const Header = () => {
   return (
     <Section className={classNames(styles.header)}>
-      <div className='hero-body'>
-        <div className='container'>
+      <HeroBody className='hero-body'>
+        <HeroWrapper>
           <div>
             <h1 className={classNames('title mt-6', styles.title)}>
               let's create software that <span>matters</span>
             </h1>
           </div>
-          <div className='buttons is-hidden-tablet'>
-            <a className='button estimate is-primary' href={routeLinks.startProject}>
-              <strong>estimate project</strong>
-            </a>
-          </div>
+        </HeroWrapper>
+        <div className='buttons is-hidden-tablet'>
+          <a className='button estimate is-primary' href={routeLinks.startProject}>
+            <strong>estimate project</strong>
+          </a>
         </div>
-      </div>
+      </HeroBody>
     </Section>
   )
 }
