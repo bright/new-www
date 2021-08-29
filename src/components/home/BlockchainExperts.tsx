8420181002ffb2aeafc6b99ff0644ff2c63a227c
codersbros <p.grden@codersbros.pl>
2021-08-29 17:21:20 +0200

    create blockchainExperts component

---
+++ src/components/home/BlockchainExperts.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
+
+export const BlockchainExperts = () => {
+  return (
+    <>
+      <h3>we are blockchain experts</h3>
+      <p>
+        We have experience in developing blockchain projects in Ethereum and Substrate. We believe that blockchain is
+        the technology of the future that’s why we’ve developed our original project for blockchain enthusiasts as well.
+        Meet Bright Treasury – our original app built to support blockchain Substrate community.
+      </p>
+      <StaticImage src='' alt='' placeholder='blurred' layout='fixed' width={200} height={200} className='about-img' />
+      <StaticImage src='' alt='' placeholder='blurred' layout='fixed' width={200} height={200} className='about-img' />
+      <button>estimate blockchain project</button>
+    </>
+  )
+}
