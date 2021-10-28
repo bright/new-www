e87904ed433de429dc1df5a6342cd5969f91d092
codersbros <p.grden@codersbros.pl>
2021-10-28 22:40:03 +0200

    create new deviceSize 1280px

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -4,6 +4,7 @@ export const pxToRem = (px: number) => `${px / basePx}rem`
 const deviceSize = {
   mobile: 580,
   tablet: 991,
+  tabletXL: 1280,
   laptop: 1540,
   desktop: 1920,
 
@@ -17,6 +18,7 @@ export default {
   size: {
     mobile: `${deviceSize.mobile}px`,
     tablet: `${deviceSize.tablet}px`,
+    tabletXL: `${deviceSize.tabletXL}px`,
     laptop: `${deviceSize.laptop}px`,
     desktop: `${deviceSize.desktop}px`,
     navHeight: '4.6rem'
@@ -25,6 +27,7 @@ export default {
   device: {
     mobile: `screen and (max-width: ${deviceSize.mobile +1}px)`,
     tablet: `screen and (max-width: ${deviceSize.tablet +1}px)`,
+    tabletXL: `screen and (max-width: ${deviceSize.tabletXL +1}px)`,
     laptop: `screen and (max-width: ${deviceSize.laptop +1}px)`,
     desktop: `screen and (max-width: ${deviceSize.desktop +1}px)`,
   },
