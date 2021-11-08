3a24a8c19206ac3e57c888d60b1b4d833f7a58c6
codersbros <p.grden@codersbros.pl>
2021-11-08 08:47:59 +0100

    adaptation RecrutingProcess to four resolution

--- src/pages/career/RecruitingProcess.tsx
+++ src/pages/career/RecruitingProcess.tsx
@@ -2,14 +2,31 @@ import React, { useMemo } from 'react'
 import styled from 'styled-components'
 
 import { Section, SectionTitle } from '../../components/shared'
+import { CustomSectionTitle } from '../../components/shared/index.styled'
 import variables from '../../styles/variables'
 
-const RecruitingTitle = styled(SectionTitle)`
+const RecruitingSection = styled(Section)`
+  padding: 2rem 15rem 6rem 15rem;
+  @media ${variables.device.laptop} {
+    padding: 0 6rem 1.625rem 6rem;
+  }
+  @media ${variables.device.tabletXL} {
+    padding: 0 9rem 1.625rem 9rem;
+  }
+  @media ${variables.device.tablet} {
+    padding: 0 2.25rem 1.625rem 2.25rem;
+  }
+`
+
+const RecruitingTitle = styled(CustomSectionTitle)`
   margin-bottom: 4rem;
   font-size: 2.5rem;
-  line-height: 3.75rem;
-  font-weight: 800;
-
+  line-height: 3.125rem;
+  font-weight: 900;
+  @media ${variables.device.laptop} {
+    font-size: 2.125rem;
+    line-height: 2.625rem;
+  }
   @media ${variables.device.mobile} {
     font-size: 1.375rem;
   }
@@ -67,28 +84,86 @@ const Columns = styled.div`
       font-weight: 700;
     }
   }
-
-  @media ${variables.device.mobile} {
+  @media ${variables.device.laptop} {
+    & .column {
+      &:not(.is-half):after {
+        width: 1.0625rem;
+        height: 0.625rem;
+      }
+      &:last-of-type p {
+        font-size: 1.3125rem;
+        margin-top: 3rem;
+      }
+      figure img {
+        max-height: 11rem;
+        object-fit: contain;
+      }
+      p {
+        font-size: 1rem;
+      }
+    }
+  }
+  @media ${variables.device.tabletXL} {
+    & .column {
+      &:not(.is-half):after {
+        width: 0.8125rem;
+        height: 0.5rem;
+      }
+      &:last-of-type p {
+        font-size: 1.125rem;
+        margin-top: 2.3125rem;
+      }
+      figure img {
+        max-height: 9rem;
+        object-fit: contain;
+      }
+      p {
+        font-size: 0.875rem;
+        margin-top: 1.25rem;
+      }
+    }
+  }
+  @media ${variables.device.tablet} {
+    display: block !important;
     && {
       .column {
         margin-bottom: 4.5625rem;
         padding: 0 1.125rem;
+        width: 100%;
 
-        &:after {
-          display: block;
+        &:not(.is-half):after {
           top: unset;
-          bottom: -2rem;
-          transform: translateX(-50%) rotate(90deg);
+          bottom: -3rem;
+          width: 1.5rem;
+          height: 1rem;
+          transform: translateX(-50%) rotate(90deg) scale(1.5);
           left: 50%;
           right: unset;
         }
 
-        figure {
-          height: auto;
+        figure img {
+          max-height: 296px;
+          width: 100%;
           margin: 0 auto;
         }
         p {
-          font-size: 1.125rem;
+          font-size: 1.375rem;
+          margin-top: 2.5rem;
+        }
+      }
+    }
+    @media ${variables.device.mobile} {
+      && {
+        .column {
+          &:not(.is-half):after {
+            top: unset;
+            bottom: -3rem;
+            width: 1.5rem;
+            height: 1rem;
+            transform: translateX(-50%) rotate(90deg) scale(1);
+            left: 50%;
+            right: unset;
+          }
         }
       }
     }
@@ -110,7 +185,7 @@ const RecruitingProcess: React.FC = () => {
   )
 
   return (
-    <Section>
+    <RecruitingSection>
       <RecruitingTitle>recruiting process</RecruitingTitle>
 
       <Columns className='columns is-multiline has-justify-content-center'>
@@ -131,7 +206,7 @@ const RecruitingProcess: React.FC = () => {
           <p>5. congrats! you are a part of a bright team!</p>
         </div>
       </Columns>
-    </Section>
+    </RecruitingSection>
   )
 }
 
