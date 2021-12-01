23ac586d2ed0c62f0c4cc01df8beb4992dd1fec9
codersbros <p.grden@codersbros.pl>
2021-12-01 18:05:51 +0100

    create custom style for our development areas

--- src/components/whatWeDo/ourDevelopmentAreas/styles.ts
+++ src/components/whatWeDo/ourDevelopmentAreas/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import { TextRegular, TextTitle } from '../../shared'
 import variables from '../../../styles/variables'
 import { Link } from 'gatsby'
+import { pxToRem } from './../../../styles/variables'
 
 export const DevelopmentAreasWrapper = styled.div({
   display: 'flex',
@@ -18,25 +19,11 @@ export const DevelopmentAreasContainer = styled.div({
   maxWidth: '1344px',
 })
 
-export const SectionTitleContainer = styled.div<{
-  iconMobileWidth: string
-  iconMobileHeight: string
-}>(({ iconMobileWidth, iconMobileHeight }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-
-  [`@media screen and (max-width: 767px)`]: {
-    svg: {
-      width: iconMobileWidth,
-      height: iconMobileHeight,
-    },
-  },
-}))
-
 export const Title = styled(TextTitle)({
   marginTop: '36px',
   marginBottom: '54px',
+  fontSize: '22px',
+  lineHeight: '27px',
 
   [`@media screen and (max-width: 767px)`]: {
     marginTop: '30px',
@@ -56,23 +43,6 @@ export const SectionText = styled(TextRegular)({
   },
 })
 
-export const DevelopmentAreaContainer = styled.div({
-  flexGrow: 0,
-  flexShrink: 1,
-
-  marginTop: '65px',
-
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-
-  padding: '10px 30px',
-
-  [`@media screen and (min-width: 767px)`]: {
-    flexBasis: '50%',
-  },
-})
-
 export const GoToContainer = styled.div({
   marginTop: '18px',
 })
@@ -81,5 +51,44 @@ export const RevertHoverLink = styled(Link)`
   color: #363636;
   &:hover {
     color: ${variables.color.primary};
+  }
+`
+export const DevelopmentAreaContainer = styled.div`
+  flex-grow: 0;
+  flex-shrink: 1;
+  margin-top: ${variables.pxToRem(65)};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: ${variables.pxToRem(10)} ${variables.pxToRem(30)};
+  flex-basis: 50%;
+
+  & p {
+    font-family: ${variables.font.customtext.lato};
+    font-size: ${variables.pxToRem(22)};
+    line-height: ${variables.pxToRem(40)};
+    color: #000000;
+    opacity: 0.75;
+  }
+
+  @media ${variables.device.tablet} {
+    flex-basis: 100%;
+    & p {
+      font-size: ${variables.pxToRem(18)};
+      line-height: ${variables.pxToRem(32)};
+    }
+  }
+`
+export const SectionTitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media ${variables.device.tablet} {
+    & .about-img {
+      & .image {
+        max-height: ${variables.pxToRem(75)};
+        width: auto;
+      }
+    }
   }
 `
