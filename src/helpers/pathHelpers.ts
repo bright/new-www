a7a60f1024656844080db104c9495f08a6c4f08e
codersbros <p.grden@codersbros.pl>
2021-12-07 20:04:02 +0100

    remove question mark in kebabCase

--- src/helpers/pathHelpers.ts
+++ src/helpers/pathHelpers.ts
@@ -1,5 +1,5 @@
 export const getUrlForAbsolutePath = (path: string) => {
-  return path.split("pages").pop().replace(".md", "")
+  return path.split('pages').pop().replace('.md', '')
 }
 
 export const getJobPath = (path: string) => {
@@ -7,15 +7,16 @@ export const getJobPath = (path: string) => {
 }
 
 export const getFileNameOnly = (path: string) => {
-  return path.split("/").pop().replace(".md", "")
+  return path.split('/').pop().replace('.md', '')
 }
 
 export const deleteTimestampFromUrl = (path: string) => {
-  return path.replace(/([0-9]{4})-([0-9]{2})-([0-9]{2})-/, "")
+  return path.replace(/([0-9]{4})-([0-9]{2})-([0-9]{2})-/, '')
 }
 
 export const kebabCase = (string: string) =>
-string
-  .replace(/([a-z])([A-Z])/g, '$1-$2')
-  .replace(/\s+/g, '-')
-  .toLowerCase()
+  string
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .replace(/\s+/g, '-')
+    .replace(/[?=]/g, '')
+    .toLowerCase()
