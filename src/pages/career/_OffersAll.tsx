e866ceccddcdb5a9b86179eff7d0cab91767e87e
codersbros <p.grden@codersbros.pl>
2021-12-16 14:25:14 +0100

    change skip in jobsQuery OfferAll

--- src/pages/career/_OffersAll.tsx
+++ src/pages/career/_OffersAll.tsx
@@ -1,19 +1,17 @@
-import React  from 'react'
-import { graphql,  useStaticQuery } from 'gatsby'
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
 import { createJobs } from '../../models/creator'
 import OffersList from './_OffersList'
 
-const OffersAll: React.FC = () => <OffersList jobs={createJobs(useStaticQuery(jobsQuery))}/>
+const OffersAll: React.FC = () => <OffersList jobs={createJobs(useStaticQuery(jobsQuery))} />
 
 const jobsQuery = graphql`
   query {
     allMarkdownRemark(
-      filter: {
-        frontmatter: { layout: { eq: "job" }, published: { eq: true } }
-      }
-      skip: 3,
-      sort: {fields: [frontmatter___order]}
+      filter: { frontmatter: { layout: { eq: "job" }, published: { eq: true } } }
+      skip: 4
+      sort: { fields: [frontmatter___order] }
     ) {
       edges {
         node {
