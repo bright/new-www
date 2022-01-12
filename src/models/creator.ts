d67643d2a33cc74fc642bebaba453ebaaae9a759
codersbros <p.grden@codersbros.pl>
2022-01-12 13:11:13 +0100

    add array to creator

--- src/models/creator.ts
+++ src/models/creator.ts
@@ -1,9 +1,9 @@
-import { BlogPostModel, ProjectModel, GQLData, AuthorModel, JobModel } from "./gql"
+import { BlogPostModel, ProjectModel, GQLData, AuthorModel, JobModel } from './gql'
 
-export const createBlogPosts = (data: GQLData): BlogPostModel[] =>
-  (data.allMarkdownRemark.edges || []).map(({ node }) => {
-      const base = node.frontmatter
-      return {
+export const createBlogPosts = (data: GQLData): BlogPostModel[] => {
+  return (data.constructor === Array ? data : data.allMarkdownRemark?.edges || []).map(({ node }) => {
+    const base = node.frontmatter
+    return {
       ...base,
       id: node.id,
       slug: node.fields.slug,
@@ -11,20 +11,21 @@ export const createBlogPosts = (data: GQLData): BlogPostModel[] =>
       tags: base.tags ?? [],
     } as BlogPostModel
   })
+}
 
-export const createProjects = (data: GQLData): ProjectModel[] => (
+export const createProjects = (data: GQLData): ProjectModel[] =>
   (data.allMarkdownRemark.edges || [])
     .map(({ node: { frontmatter } }: { node: { frontmatter: ProjectModel } }) => frontmatter)
     .filter((project: ProjectModel) => project.published)
     .sort((a: ProjectModel, b: ProjectModel) => (a.order || 99) - (b.order || 99))
-)
 
-export const createAuthors = (data: GQLData): AuthorModel[] => (
-  (data.allMarkdownRemark.nodes || [])
-    .map(({ frontmatter }: { frontmatter: AuthorModel }) => frontmatter)
-)
+export const createAuthors = (data: GQLData): AuthorModel[] =>
+  (data.allMarkdownRemark.nodes || []).map(({ frontmatter }: { frontmatter: AuthorModel }) => frontmatter)
 
-export const createJobs = (data: GQLData): JobModel[] => (
-  (data.allMarkdownRemark.edges || [])
-    .map(({ node: { frontmatter, fileAbsolutePath } }: { node: { frontmatter: JobModel, fileAbsolutePath: string } }) => ({...frontmatter, url: fileAbsolutePath}))
-)
+export const createJobs = (data: GQLData): JobModel[] =>
+  (data.allMarkdownRemark.edges || []).map(
+    ({ node: { frontmatter, fileAbsolutePath } }: { node: { frontmatter: JobModel; fileAbsolutePath: string } }) => ({
+      ...frontmatter,
+      url: fileAbsolutePath,
+    })
+  )
