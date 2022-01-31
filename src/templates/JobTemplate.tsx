50111b25ff9df40dda76bc995939fe0673d74a99
codersbros <p.grden@codersbros.pl>
2022-01-31 11:01:57 +0100

    implementation use-scrollposition and adaptation to resolution JobTemplate

--- src/templates/JobTemplate.tsx
+++ src/templates/JobTemplate.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useMemo, MutableRefObject } from 'react'
 import { graphql, Link } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { BlackButton } from '../components/about-us/about-us.styled'
@@ -8,10 +8,15 @@ import { routeLinks } from '../config/routing'
 import { HelmetTitleDescription } from '../meta/HelmetTitleDescription'
 import RecruitingProcess from '../pages/career/_RecruitingProcess'
 import styled from 'styled-components'
-import { CustomPageTitle, CustomConstrainedWidthContainer, SectionInner } from '../components/shared/index'
+import { CustomPageTitle, SectionInner, CustomSectionInner } from '../components/shared/index'
 import { FormComponent } from '../components/about-us/form-section/form'
 import variables from '../styles/variables'
 import AnalyticsLink from '../components/shared/AnalyticsLink'
+import { ArrowJobTemplateIcon } from '../components/icons/ArrowJobTemplate.icon'
+import { CustomSection } from './../components/shared/index'
+import { useScrollPosition } from '../components/utils/use-scrollposition'
+
+type ElementRef = MutableRefObject<HTMLElement | undefined>
 
 const SalaryHeading = styled.h5`
   margin: 1.5rem 0;
@@ -31,8 +36,7 @@ const SalaryWrapper = styled.div`
     font-family: 'Lato', sans-serif;
     font-size: 1.5rem;
     line-height: 1.75rem;
-    opacity: 0.75;
-    color: #000;
+    color: ${variables.color.text2};
     flex-basis: 50%;
     &:last-of-type {
       padding-left: 1rem;
@@ -79,8 +83,7 @@ const HoursWraper = styled.div`
   > h4 {
     position: relative;
     flex-basis: 50%;
-    color: #000;
-    opacity: 0.75;
+    color: ${variables.color.black2};
     text-align: end;
     font-family: inherit;
     line-height: inherit;
@@ -105,8 +108,11 @@ const HoursWraper = styled.div`
 `
 const RecruitingProcessWrappers = styled.div`
   padding: 0 5rem;
+  @media ${variables.device.laptop} {
+    padding: 0;
+  }
   @media ${variables.device.mobile} {
-    padding: 0 2.8rem;
+    padding: 0 0.5rem;
   }
 `
 const JobBlackButton = styled(BlackButton)`
@@ -134,27 +140,29 @@ const JobBackButton = styled(BackButton)`
   }
 `
 const JobSectionInner = styled(SectionInner)`
-  & > .content {
+  && .content {
     h2 {
-      margin-bottom: 4rem;
-      margin-top: 6.5rem;
+      margin-bottom: ${variables.pxToRem(64)};
+      margin-top: ${variables.pxToRem(105)};
       font-size: 2rem;
       line-height: 2.5rem;
-      color: #000;
+      color: ${variables.color.black2};
+      font-weight: 900;
       text-align: center;
     }
     p {
-      font-size: 1.375rem;
+      font-size: ${variables.pxToRem(20)};
       line-height: 2.5rem;
+      color: ${variables.color.text2};
     }
     ul > li {
       position: relative;
-      margin-bottom: 3rem;
+      margin-bottom: ${variables.pxToRem(37)};
+      margin-top: 0;
       list-style: none;
-      font-size: 1.375rem;
+      font-size: ${variables.pxToRem(20)};
       line-height: 2rem;
-      color: #000;
-      opacity: 0.75;
+      color: ${variables.color.text2};
       &:before {
         content: '';
         display: block;
@@ -169,23 +177,44 @@ const JobSectionInner = styled(SectionInner)`
         background-position: center;
       }
     }
+    @media ${variables.device.laptop} {
+      h2 {
+        margin-top: ${variables.pxToRem(82)};
+      }
+    }
+    @media ${variables.device.tablet} {
+      h2 {
+        margin-top: ${variables.pxToRem(64)};
+      }
+    }
     @media ${variables.device.mobile} {
       p {
         font-size: 1rem;
         line-height: 1.75rem;
       }
       h2 {
-        margin-bottom: 2.25rem;
-        margin-top: 4rem;
+        margin-bottom: ${variables.pxToRem(36)};
+        margin-top: ${variables.pxToRem(64)};
         font-size: 1.375rem;
         line-height: 1.7rem;
       }
+      ul {
+        &:last-of-type {
+          margin-bottom: ${variables.pxToRem(64)};
+        }
+      }
       ul > li {
-        margin-bottom: 2.875rem;
+        margin-bottom: ${variables.pxToRem(30)};
         font-size: 1rem;
         line-height: 1.75rem;
         &:before {
           left: calc(-47.5px + 18px);
+          background-image: url('/images/okmobile.svg');
+          width: 13px;
+          height: 13px;
+        }
+        &:last-of-type {
+          margin-bottom: ${variables.pxToRem(0)};
         }
       }
     }
@@ -205,7 +234,7 @@ const TechnologyWrapper = styled.div`
       font-size: 1.375rem;
       font-weight: 600;
       line-height: 1.687;
-      color: #131214;
+      color: ${variables.color.text2};
       padding: 1rem 1.125rem;
       border: 1px solid rgba(211, 211, 211, 0.47);
       @media ${variables.device.mobile} {
@@ -234,11 +263,34 @@ const JobFormComponent = styled(FormComponent)`
   }
 `
 const ImageWrapper = styled.div`
+  position: relative;
+  @media ${variables.device.tablet} {
+    padding-left: ${variables.pxToRem(36)};
+    width: 100vw;
+    overflow: scroll;
+  }
   @media ${variables.device.mobile} {
-    width: auto;
-    overflow-x: scroll;
-    & > .about-img {
-      width: 1000px;
+    padding-left: ${variables.pxToRem(18)};
+  }
+`
+const ImageWrapperRef = styled.div`
+  width: 100%;
+  @media ${variables.device.tablet} {
+    & .scroll {
+      display: inline-flex;
+      & > .about-img {
+        width: 1900px;
+        & img {
+          z-index: -1;
+        }
+      }
+    }
+  }
+  @media ${variables.device.mobile} {
+    & .scroll {
+      & > .about-img {
+        width: 1000px;
+      }
     }
   }
 `
@@ -251,6 +303,47 @@ const ButtonWrapper = styled.div`
     width: 100%;
     margin: 0;
     padding: 0.8125rem 0;
+  }
+`
+const SvgArrowWrapper = styled.div<{ show: boolean; isRotate: boolean }>`
+  position: sticky;
+  right: 1rem;
+
+  opacity: 0;
+  height: fit-content;
+
+  @media ${variables.device.tablet} {
+    transform: ${({ isRotate }) => (isRotate ? 'rotate(180deg)' : 'rotate(0deg)')};
+    opacity: ${({ show }) => (show ? '1' : '0')};
+    right: ${variables.pxToRem(37)};
+    margin-top: ${variables.pxToRem(305)};
+  }
+  @media ${variables.device.mobile} {
+    right: ${variables.pxToRem(18)};
+    margin-top: ${variables.pxToRem(173)};
+  }
+`
+const WrapperJobBackButton = styled.div`
+  display: flex;
+  margin: 0 auto;
+  padding-bottom: ${variables.pxToRem(186)};
+  & span {
+    font-size: ${variables.pxToRem(18)};
+  }
+  @media ${variables.device.laptop} {
+    padding-bottom: ${variables.pxToRem(117)};
+    & span {
+      font-size: ${variables.pxToRem(16)};
+    }
+  }
+  @media ${variables.device.tabletXL} {
+    padding-bottom: ${variables.pxToRem(116)};
+  }
+  @media ${variables.device.mobile} {
+    padding-bottom: ${variables.pxToRem(80)};
+    & span {
+      font-size: ${variables.pxToRem(18)};
+    }
   }
 `
 
@@ -286,62 +379,116 @@ export default function Template({
   const listTechnologies = technologies?.map(technology => <li key={technology}>{technology}</li>)
 
   const image = getImage(page.imagejob)!
-  return (
-    <Page>
-      <HelmetTitleDescription title={page.title} description={page.description} />
-      <CustomConstrainedWidthContainer>
-        <CustomPageTitle>{page.title}</CustomPageTitle>
-        <SalaryHeading className='has-text-primary'>{page.subtitle}</SalaryHeading>
-        <SalaryWrapper>
-          <Salary salary={page.salary} />
-        </SalaryWrapper>
-        <HoursWraper>
-          {page.working_time && <h4>{page.working_time}</h4>}
-          <span>Gdańsk</span>
-        </HoursWraper>
-        <TechnologyWrapper>
-          <ul>{listTechnologies ? listTechnologies : <li></li>}</ul>
-        </TechnologyWrapper>
-        <ButtonWrapper>
-          <a href='#jobform'>
-            <JobBlackButton>{page.button}</JobBlackButton>
-          </a>
-        </ButtonWrapper>
-        <ImageWrapper>
-          <GatsbyImage image={image} alt={page.image_alt_job} className='about-img' quality='100' />
+  const [hideOnScroll, setHideOnScroll] = useState<boolean>(false)
+  const [rotateArrow, setRotateArrow] = useState<boolean>(false)
+
+  const element = React.useRef<HTMLDivElement>(null)
+  const boundingElement = React.useRef<HTMLDivElement>(null)
+
+  useScrollPosition(
+    ({ prevPos, currPos, isScrolling }) => {
+      if (boundingElement.current && element.current && element.current.firstChild) {
+        const child: HTMLDivElement = element.current.firstChild as HTMLDivElement
+        const { clientWidth: parentWidth } = boundingElement.current
+        const { clientWidth: childWidth } = child
+        const isEndOfScroll = currPos.x + parentWidth === childWidth
+
+        if (isEndOfScroll) {
+          setRotateArrow(true)
+        } else if (currPos.x < 0) {
+          setRotateArrow(false)
+        }
+        setTimeout(() => {
+          setHideOnScroll(isScrolling)
+        }, 0)
+      }
+    },
+    [],
+    element as MutableRefObject<HTMLElement | undefined>,
+    false,
+    0,
+    boundingElement as MutableRefObject<HTMLElement | undefined>
+  )
+  return useMemo(
+    () => (
+      <Page>
+        <HelmetTitleDescription title={page.title} description={page.description} />
+        <CustomSection
+          paddingMobileProps='64px 18px 0'
+          paddingTablet='83px 36px 0'
+          paddingTabletXL='59px 144px 0'
+          paddingLaptop='55px 96px 0'
+          paddingProps='67px 240px 0'
+        >
+          <CustomPageTitle>{page.title}</CustomPageTitle>
+          <SalaryHeading className='has-text-primary'>{page.subtitle}</SalaryHeading>
+          <SalaryWrapper>
+            <Salary salary={page.salary} />
+          </SalaryWrapper>
+          <HoursWraper>
+            {page.working_time && <h4>{page.working_time}</h4>}
+            <span>Gdańsk</span>
+          </HoursWraper>
+          <TechnologyWrapper>
+            <ul>{listTechnologies ? listTechnologies : <li></li>}</ul>
+          </TechnologyWrapper>
+          <ButtonWrapper>
+            <a href='#jobform'>
+              <JobBlackButton>{page.button}</JobBlackButton>
+            </a>
+          </ButtonWrapper>
+        </CustomSection>
+
+        <ImageWrapper ref={boundingElement}>
+          <ImageWrapperRef ref={element}>
+            <div className='scroll'>
+              <GatsbyImage image={image} alt={page.image_alt_job} className='about-img' quality='100' />
+              <SvgArrowWrapper className='arrow-wrapper' isRotate={rotateArrow} show={!hideOnScroll}>
+                <ArrowJobTemplateIcon />
+              </SvgArrowWrapper>
+            </div>
+          </ImageWrapperRef>
         </ImageWrapper>
-        <JobSectionInner>
-          <div className='content' dangerouslySetInnerHTML={{ __html: html }} />
-        </JobSectionInner>
+
+        <CustomSection>
+          <CustomSectionInner tabletXLMaxWidth='754px' laptopMaxWidth='754px' maxWidth='754px'>
+            <JobSectionInner>
+              <div className='content' dangerouslySetInnerHTML={{ __html: html }} />
+            </JobSectionInner>
+          </CustomSectionInner>
+        </CustomSection>
         <RecruitingProcessWrappers>
           <RecruitingProcess />
         </RecruitingProcessWrappers>
-        <SectionInner id='jobform'>
-          <JobFormComponent
-            style={{ marginTop: '0', marginBottom: '5rem' }}
-            title={'submit your application'}
-            description={
-              <>
-                You can either use our form below or send your application directly via email{' '}
-                <AnalyticsLink
-                  href='mailto:ula@bright.dev'
-                  action='Click Recruitment Email'
-                  category='Click Email'
-                  text='ula@bright.dev'
-                />
-                . Feel free to ask any questions on the position and the project.
-              </>
-            }
-            namePlaceholder={'Enter name here'}
-            mailPlaceholder={'name@mail.com'}
-            textPlaceholder={'Let us know what would you like to do @ bright inventions'}
-            uploadLabel={'Upload '}
-          />
-        </SectionInner>
-        <JobBackButton label='back to career' url={`${routeLinks.career}`} arrowColor='orange' />
-      </CustomConstrainedWidthContainer>
+        <CustomSection>
+          <CustomSectionInner id='jobform' tabletXLMaxWidth='754px' laptopMaxWidth='754px' maxWidth='754px'>
+            <JobFormComponent
+              style={{ marginTop: '0', marginBottom: '5rem' }}
+              title={'submit your application'}
+              description={
+                <>
+                  You can either use our form below or send your application directly via email{' '}
+                  <AnalyticsLink
+                    href='mailto:ula@bright.dev'
+                    action='Click Recruitment Email'
+                    category='Click Email'
+                    text='ula@bright.dev'
+                  />
+                  . Feel free to ask any questions on the position and the project.
+                </>
+              }
+              namePlaceholder={'Enter name here'}
+              mailPlaceholder={'name@mail.com'}
+              textPlaceholder={'Let us know what would you like to do @ bright inventions'}
+              uploadLabel={'Upload '}
+            />
+          </CustomSectionInner>
+        </CustomSection>
+        <WrapperJobBackButton>
+          <JobBackButton label='back to career' url={`${routeLinks.career}`} arrowColor='orange' />
+        </WrapperJobBackButton>
 
-      {/* <script type="application/ld+json">
+        {/* <script type="application/ld+json">
     {
         "@context": "https://schema.org/",
         "@type": "JobPosting",
@@ -382,7 +529,9 @@ export default function Template({
     }
 </script>
   */}
-    </Page>
+      </Page>
+    ),
+    [rotateArrow, hideOnScroll]
   )
 }
 export const pageQuery = graphql`
