8fe526d5780545b5ee28b77d01755be077a2f55b
codersbros <p.grden@codersbros.pl>
2022-02-02 23:36:36 +0100

    add new color font to variables.ts

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -33,12 +33,12 @@ export default {
 
   color: {
     primary: '#f7931e', //brightOrange
-    text: '#131214',
+    text: '#0A0A0A',
     text2: '#0A0A0A',
     border: '#d3d3d3',
     white: '#ffffff',
     black: '#000000',
-    black2: '#0A0A0A;',
+    heading: '#0A0A0A;',
   },
 
   font: {
