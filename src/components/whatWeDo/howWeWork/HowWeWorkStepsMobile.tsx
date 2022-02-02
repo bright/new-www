0754518b9d305ff39e54c416ecd36d76ab7ab0c7
codersbros <p.grden@codersbros.pl>
2022-02-02 23:31:29 +0100

    change color font in HowWeworkStepsMobile

--- src/components/whatWeDo/howWeWork/HowWeWorkStepsMobile.tsx
+++ src/components/whatWeDo/howWeWork/HowWeWorkStepsMobile.tsx
@@ -1,38 +1,38 @@
-import React from "react"
-import styled from "styled-components"
-import { TextTitle } from "../../shared"
-import variables from "../../../styles/variables"
+import React from 'react'
+import styled from 'styled-components'
+import { TextTitle } from '../../shared'
+import variables from '../../../styles/variables'
 
 const Container = styled.div({
-  marginTop: "99px",
-  width: "100%",
-  height: "318px",
-  background: "#F7931E 0% 0% no-repeat padding-box",
+  marginTop: '99px',
+  width: '100%',
+  height: '318px',
+  background: '#F7931E 0% 0% no-repeat padding-box',
 
-  display: "flex",
-  flexDirection: "column",
+  display: 'flex',
+  flexDirection: 'column',
 
-  justifyContent: "center",
-  alignItems: "center",
+  justifyContent: 'center',
+  alignItems: 'center',
 })
 
 const ProcessImage = styled.img({
-  marginTop: "29px",
-  paddingLeft: "18px",
-  paddingRight: "18px",
+  marginTop: '29px',
+  paddingLeft: '18px',
+  paddingRight: '18px',
 })
 
 const Header = styled(TextTitle)({
-  textAlign: "center",
+  textAlign: 'center',
 
-  color: variables.color.black,
+  color: variables.color.text,
 })
 
 const HowWeWorkStepsMobile = () => {
   return (
     <Container>
       <Header>the process</Header>
-      <ProcessImage src="/images/WhatWeDoArrowsMobile.png" />
+      <ProcessImage src='/images/WhatWeDoArrowsMobile.png' />
     </Container>
   )
 }
