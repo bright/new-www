a853423cf36323d57e5a9c0a444879b931a579f2
codersbros <p.grden@codersbros.pl>
2022-02-08 13:23:26 +0100

    change font size in story.tsx

--- src/components/about-us/story-section/story.tsx
+++ src/components/about-us/story-section/story.tsx
@@ -1,24 +1,26 @@
-import React from "react"
-import styled from "styled-components"
+import React from 'react'
+import styled from 'styled-components'
 
-import Timeline from "../../timeline"
-import { story } from "./story-data"
+import Timeline from '../../timeline'
+import { story } from './story-data'
 import { TextRegular } from '../../shared'
+import variables from '../../../styles/variables'
 
 const Container = styled.p`
   max-width: 960px;
   margin: auto;
 `
+const StoryTextRegular = styled(TextRegular)`
+  font-size: ${variables.pxToRem(18)};
+`
 
 export function StoryComponent() {
   return (
-    <Container className="tab-content content container">
+    <Container className='tab-content content container'>
       <TextRegular>
-        Bright Inventions is a software consulting studio based in Gdansk,
-        Poland. Since 2012 we have built software for more than 40 businesses
-        worldwide. Our expertise in mobile, web, blockchain and IoT systems has
-        been highly appreciated by our clients from UK, Germany, Netherlands,
-        Norway Israel and more.
+        Bright Inventions is a software consulting studio based in Gdansk, Poland. Since 2012 we have built software for
+        more than 40 businesses worldwide. Our expertise in mobile, web, blockchain and IoT systems has been highly
+        appreciated by our clients from UK, Germany, Netherlands, Norway Israel and more.
       </TextRegular>
       <Timeline>
         {story.map(item => {
@@ -35,7 +37,7 @@ export function StoryComponent() {
               </Timeline.Heading>
               <Timeline.Subheading>{item.subheading}</Timeline.Subheading>
               <Timeline.Content>
-                <TextRegular>{item.content}</TextRegular>
+                <StoryTextRegular>{item.content}</StoryTextRegular>
               </Timeline.Content>
             </Timeline.Element>
           )
