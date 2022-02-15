a80b7a7e77e081c83494c796b417be79bc8b32a6
codersbros <p.grden@codersbros.pl>
2022-02-15 11:22:38 +0100

    add new text to HowWeWorkDescription and adaptation to  resolution

--- src/components/whatWeDo/howWeWork/HowWeWorkDescription.tsx
+++ src/components/whatWeDo/howWeWork/HowWeWorkDescription.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 import variables from '../../../styles/variables'
-import { SectionTitle, TextRegular } from '../../shared'
+import { CustomSectionTitle, SectionTitle, TextRegular, CustomSectionInner } from '../../shared'
 
 const Container = styled.div({
   color: variables.color.text,
-
-  marginTop: '131px',
 
   display: 'flex',
   justifyContent: 'center',
@@ -18,18 +16,23 @@ export const ContentWrapper = styled.div({
 
   display: 'flex',
   flexDirection: 'column',
-  alignItems: 'flex-start',
 
   padding: '0px 20px',
+  [`@media screen and (max-width: 540px)`]: {
+    padding: '0px 18px',
+  },
 })
 
-export const ParagraphTextWrapper = styled(TextRegular)({
-  marginTop: '55px',
+export const ParagraphTextWrapper = styled(TextRegular)`
+  margin-top: 55px;
 
-  textAlign: 'left',
+  text-align: left;
 
-  color: variables.color.text,
-})
+  color: ${variables.color.text};
+  & :first-child {
+    margin-top: 0;
+  }
+`
 
 export const ParagraphHeader = styled(SectionTitle)({
   marginTop: '105px',
@@ -39,33 +42,28 @@ const HowWeWorkDescription = () => {
   return (
     <Container>
       <ContentWrapper>
-        <ParagraphTextWrapper>
-          We help shape our clients’ success stories at all software development stages – from ideation and
-          requirements’ elicitation, through product design and bespoke software development, to quality assurance and
-          maintenance.
-        </ParagraphTextWrapper>
-        <ParagraphTextWrapper>
-          Regardless of the project development stage you’re in, we’re here to help bring your ideas to life.
-        </ParagraphTextWrapper>
-
-        <ParagraphHeader>how we work</ParagraphHeader>
-
-        <ParagraphTextWrapper>
-          Nothing feels equally rewarding as seeing the projects we work with our clients on grow into highly successful
-          ventures. We firmly believe that we can contribute to the business success of a project by offering our
-          expertise during all its development phases.
-        </ParagraphTextWrapper>
-        <ParagraphTextWrapper>
-          At Bright Inventions, we put communication and transparency at the core and aim at being much more than just
-          external consultants entrusted with a job. We also leverage our strategic location in the heart of Europe to
-          combine remote, daily work with on-site collaboration with partners whenever need be.
-        </ParagraphTextWrapper>
-        <ParagraphTextWrapper>
-          As our projects come to an end, we happily give our clients a helping hand in growing their internal software
-          development teams to take over product development long-term. We’re there for you at all project development
-          stages – after all, that’s what business partners are for!
-        </ParagraphTextWrapper>
-        <ParagraphTextWrapper>Here’s a step-by-step breakdown of our process:</ParagraphTextWrapper>
+        <CustomSectionTitle
+          margin='11.625rem 0 4rem'
+          laptopMargin='7.25rem 0 4rem'
+          mobileMargin='5.1875rem 1.125rem 3rem'
+        >
+          how we work
+        </CustomSectionTitle>
+        <CustomSectionInner>
+          <ParagraphTextWrapper>
+            At Bright Inventions, we contribute to the business success of a project by offering our expertise during
+            all its development phases.
+          </ParagraphTextWrapper>
+          <ParagraphTextWrapper>
+            We leverage our strategic location in Poland – the heart of Europe – to combine remote, daily work with
+            on-site collaboration with partners whenever need be.
+          </ParagraphTextWrapper>
+          <ParagraphTextWrapper>
+            As our projects come to an end, we happily give our clients a helping hand in growing their internal
+            software development teams to take over product development long-term.
+          </ParagraphTextWrapper>
+          <ParagraphTextWrapper>We’re there for you at all project development stages:</ParagraphTextWrapper>
+        </CustomSectionInner>
       </ContentWrapper>
     </Container>
   )
