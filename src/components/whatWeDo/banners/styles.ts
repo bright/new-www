8f86345620fc7e2aec5761298af6c5797de64dba
codersbros <p.grden@codersbros.pl>
2022-02-15 11:09:19 +0100

    adaptation to resolution banners.tsx

--- src/components/whatWeDo/banners/styles.ts
+++ src/components/whatWeDo/banners/styles.ts
@@ -1,12 +1,14 @@
-import styled from "styled-components"
-import variables from "../../../styles/variables"
+import styled from 'styled-components'
+import variables from '../../../styles/variables'
+import { CustomSectionTitle } from '../../shared'
 
-const {color} = variables
+const { color } = variables
 
 export const Button = styled.button`
-  width: 230px;
+  width: 100%;
   height: 54px;
-  margin-top: 55px;
+  margin-top: 64px;
+  max-width: 332px;
 
   border: 1px solid #ffffff;
   background: transparent;
@@ -32,49 +34,60 @@ export const Button = styled.button`
     border-color: #fff;
   }
 
-  @media (max-width: 767px) {
+  @media (max-width: 540px) {
     width: 100%;
+    margin-top: ${variables.pxToRem(49)};
+    max-width: 100%
   },
 `
 
 export const Container = styled.div({
-  display: "flex",
-  justifyContent: "center",
+  display: 'flex',
+  justifyContent: 'center',
 
   backgroundColor: `${variables.color.text}`,
 
-  height: "415px",
-  width: "100%",
-  alignItems: "center",
+  height: '425px',
+  width: '100%',
+  alignItems: 'center',
 
-  marginTop: "185px",
-  ["@media screen and (max-width: 767px)"]: {
-    marginTop: "99px",
-    height: "292px",
-    textAlign: "center",
+  marginTop: '186px',
+  ['@media screen and (max-width: 1540px)']: {
+    marginTop: '116px',
   },
-  padding: "0px 20px",
+  ['@media screen and (max-width: 540px)']: {
+    marginTop: '83px',
+    height: '292px',
+    textAlign: 'center',
+    padding: '0px 18px',
+  },
+  padding: '0px 20px',
 })
 
 export const ContentWrapper = styled.div({
-  maxWidth: "955px",
+  maxWidth: '955px',
   backgroundColor: `${variables.color.text}`,
 
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "flex-start",
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
 
-  color: "#FFFFFF",
+  color: '#FFFFFF',
 })
 
-export const Text = styled.div({
-  fontSize: "40px",
-  lineHeight: "48px",
+export const Text = styled(CustomSectionTitle)({
+  lineHeight: '60px',
   fontWeight: 800,
   fontFamily: variables.font.title.family,
+  maxWidth: '640px',
+  textAlign: 'center',
+  color: 'white',
+  margin: '0',
 
-  ["@media screen and (max-width: 767px)"]: {
-    fontSize: "22px",
-    lineHeight: "26px",
+  ['@media screen and (max-width: 1540px)']: {
+    maxWidth: '544px',
+  },
+  ['@media screen and (max-width: 581px)']: {
+    maxWidth: '360px',
   },
 })
