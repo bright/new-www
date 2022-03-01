6418abe353bdadf1e77b24ff8456b9235b1a7867
codersbros <p.grden@codersbros.pl>
2022-03-01 08:13:35 +0100

    fix use-pagimation.ts

--- src/templates/blog/use-pagination/use-pagination.ts
+++ src/templates/blog/use-pagination/use-pagination.ts
@@ -1,72 +1,62 @@
-import React from 'react';
-import { useMemo } from 'react';
+import React from 'react'
+import { useMemo } from 'react'
 
-export const DOTS = '...';
+export const DOTS = '...'
 
 interface RangeItems {
-    start: number;
-    end: number;
+  start: number
+  end: number
 }
 
-const range = (rangeItems: RangeItems)  => {
-  const { end, start } = rangeItems;
-  let length: number = end -    start + 1;
-  return Array.from({ length }, (_, idx) => idx + start);
-};
-
+const range = (rangeItems: RangeItems) => {
+  const { end, start } = rangeItems
+  let length: number = end - start + 1
+  return Array.from({ length }, (_, idx) => idx + start)
+}
 
 interface PaginationItems {
-    totalPageCount: number;
-    pageSize: number;
-    siblingCount: number;
-    currentPage: number
+  totalPageCount: number
+  pageSize: number
+  siblingCount: number
+  currentPage: number
 }
 
-export const usePagination = (
-    paginationItems: PaginationItems
-) => {
-    const {totalPageCount, pageSize, siblingCount, currentPage } = paginationItems
+export const usePagination = (paginationItems: PaginationItems) => {
+  const { totalPageCount, pageSize, siblingCount, currentPage } = paginationItems
   const paginationRange = useMemo(() => {
-
-    const totalPageNumbers = siblingCount + 5;
+    const totalPageNumbers = siblingCount + 5
 
     if (totalPageNumbers >= totalPageCount) {
-      return range({start: 1, end: totalPageCount});
+      return range({ start: 1, end: totalPageCount })
     }
 
-    const leftSiblingIndex = Math.max(currentPage - siblingCount, 1);
-    const rightSiblingIndex = Math.min(
-      currentPage + siblingCount,
-      totalPageCount
-    );
+    const leftSiblingIndex = Math.max(currentPage - siblingCount, 1)
+    const rightSiblingIndex = Math.min(currentPage + siblingCount, totalPageCount)
 
-    const shouldShowLeftDots = leftSiblingIndex > 2;
-    const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2;
+    const shouldShowLeftDots = leftSiblingIndex >= 2
+    const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2
 
-    const firstPageIndex = 1;
-    const lastPageIndex = totalPageCount;
+    const firstPageIndex = 1
+    const lastPageIndex = totalPageCount
 
     if (!shouldShowLeftDots && shouldShowRightDots) {
-      let leftItemCount = 1 + 2 * siblingCount;
-      let leftRange = range({start: 1, end: leftItemCount});
+      let leftItemCount = 1 + 2 * siblingCount
+      let leftRange = range({ start: 1, end: leftItemCount })
 
-      return [...leftRange, DOTS, totalPageCount];
+      return [...leftRange, DOTS, totalPageCount]
     }
 
     if (shouldShowLeftDots && !shouldShowRightDots) {
-      let rightItemCount = 3 + 2 * siblingCount;
-      let rightRange = range(
-        {start: totalPageCount - rightItemCount + 1,
-        end: totalPageCount}
-      );
-      return [firstPageIndex, DOTS, ...rightRange];
+      let rightItemCount = 3 + 2 * siblingCount
+      let rightRange = range({ start: totalPageCount - rightItemCount + 1, end: totalPageCount })
+      return [firstPageIndex, DOTS, ...rightRange]
     }
 
     if (shouldShowLeftDots && shouldShowRightDots) {
-      let middleRange = range({start: leftSiblingIndex, end: rightSiblingIndex});
-      return [firstPageIndex, DOTS, ...middleRange, DOTS, lastPageIndex];
+      let middleRange = range({ start: leftSiblingIndex, end: rightSiblingIndex })
+      return [firstPageIndex, DOTS, ...middleRange, DOTS, lastPageIndex]
     }
-  }, [totalPageCount, pageSize, siblingCount, currentPage]);
+  }, [totalPageCount, pageSize, siblingCount, currentPage])
 
-  return paginationRange;
-};
+  return paginationRange
+}
