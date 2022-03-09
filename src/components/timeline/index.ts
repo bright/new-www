33ea1037de973f3514ada14c06989f25ffd4a745
codersbros <p.grden@codersbros.pl>
2022-03-09 21:29:55 +0100

    change import timmeline logo and image

--- src/components/timeline/index.ts
+++ src/components/timeline/index.ts
@@ -1,17 +1,1 @@
-import { Timeline } from "./timeline"
-import { TimelineImage, TimelineLogo } from "./timeline-images"
-import { TimelineElement } from "./timeline-element"
-import {
-  TimelineContent,
-  TimelineHeading,
-  TimelineSubheading,
-} from "./timeline-content"
-
-Timeline.Image = TimelineImage
-Timeline.Logo = TimelineLogo
-Timeline.Element = TimelineElement
-Timeline.Heading = TimelineHeading
-Timeline.Subheading = TimelineSubheading
-Timeline.Content = TimelineContent
-
-export default Timeline
+export * from './timeline-images'
