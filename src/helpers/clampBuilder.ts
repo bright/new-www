4d92c6307970a54439ff73c445116192ee87ac6a
codersbros <p.grden@codersbros.pl>
2022-03-09 21:30:25 +0100

    create clampBuilder function

---
+++ src/helpers/clampBuilder.ts
@@ -0,0 +1,19 @@
+export function clampBuilder(
+  minWidthPx: number,
+  maxWidthPx: number,
+  minFontSizePx: number,
+  maxFontSizePx: number
+): string {
+  const root = document.querySelector('html') as Element
+  const pixelsPerRem = Number(getComputedStyle(root).fontSize.slice(0, -2))
+
+  const minWidth = minWidthPx / pixelsPerRem
+  const maxWidth = maxWidthPx / pixelsPerRem
+  const maxFontSize = maxFontSizePx / pixelsPerRem
+  const minFontSize = minFontSizePx / pixelsPerRem
+
+  const slope = (maxFontSize - minFontSize) / (maxWidth - minWidth)
+  const yAxisIntersection = -minWidth * slope + minFontSize
+
+  return `clamp( ${minFontSize}rem, ${yAxisIntersection}rem + ${slope * 100}vw, ${maxFontSize}rem )`
+}
