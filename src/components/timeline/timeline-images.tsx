e379b291e088fa92dcaa4ec056fc385ca3c61626
codersbros <p.grden@codersbros.pl>
2022-03-09 20:33:15 +0100

    style timeline images

--- src/components/timeline/timeline-images.tsx
+++ src/components/timeline/timeline-images.tsx
@@ -1,11 +1,17 @@
-import React from "react"
+import React from 'react'
 
 export function TimelineImage(props: any) {
   return (
-    <img style={{ borderRadius: "100%" }} width="50" height="50" {...props} />
+    <div>
+      <img {...props} />
+    </div>
   )
 }
 
 export function TimelineLogo(props: any) {
-  return <img width="96" height="96" {...props} />
+  return (
+    <div className='logo'>
+      <img width='96' height='96' {...props} />
+    </div>
+  )
 }
