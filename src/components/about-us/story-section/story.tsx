d75d4587507cc061bf9cd573306c092e509c3047
codersbros <p.grden@codersbros.pl>
2022-03-09 20:14:15 +0100

    create story timeline on about-us page

--- src/components/about-us/story-section/story.tsx
+++ src/components/about-us/story-section/story.tsx
@@ -1,48 +1,315 @@
 import React from 'react'
 import styled from 'styled-components'
-
 import Timeline from '../../timeline'
 import { story } from './story-data'
 import { TextRegular } from '../../shared'
 import variables from '../../../styles/variables'
+import { CustomSection } from './../../shared/index'
+import { useState } from 'react'
+import { clampBuilder } from './../../../helpers/clampBuilder'
 
-const Container = styled.p`
-  max-width: 960px;
-  margin: auto;
+const StoriesWrapperScroll = styled.div`
+  @media ${variables.device.tabletXL} {
+    overflow-x: hidden;
+    height: 600px;
+  }
+  @media ${variables.device.tablet} {
+    width: 100vw;
+    overflow: scroll;
+    height: 600px;
+  }
+
+  @media ${variables.device.mobile} {
+    height: 650px;
+  }
 `
-const StoryTextRegular = styled(TextRegular)`
-  font-size: ${variables.pxToRem(18)};
+const StoriesWrapperPosition = styled.div`
+  position: relative;
+  width: 100%;
+  &::after {
+    content: '';
+    position: absolute;
+    border-bottom: 1px solid #d3d3d3;
+    width: 100%;
+    top: 125px;
+    z-index: -1;
+  }
+  @media ${variables.device.tabletXL} {
+    width: 102vw;
+  }
+  @media ${variables.device.tablet} {
+    width: 245vw;
+  }
+  @media ${variables.device.mobile} {
+    width: 440vw;
+  }
+`
+
+const StoriesWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 ${variables.pxToRem(240)};
+
+  @media ${variables.device.laptop} {
+    padding: 0 ${variables.pxToRem(90)};
+  }
+  @media ${variables.device.tabletXL} {
+    padding: 0 0 0 ${variables.pxToRem(50)};
+  }
+  @media ${variables.device.tablet} {
+    padding: 0 0 0 ${variables.pxToRem(25)};
+  }
+  @media ${variables.device.mobile} {
+    padding: 0 0 0 ${variables.pxToRem(7)};
+  }
+`
+const StoryWrapper = styled.div`
+  /* flex-basis: calc(100% / 13); */
+  position: relative;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  & .is-active {
+    border: 1px solid #f7931e;
+    font-weight: 600;
+  }
+
+  @media ${variables.device.tablet} {
+    flex-basis: 100%;
+  }
+`
+const Data = styled.p`
+  padding: ${variables.pxToRem(14)} ${variables.pxToRem(18)};
+  border: 1px solid #d3d3d3;
+  margin-bottom: ${variables.pxToRem(64)};
+  font-weight: normal;
+  line-height: ${variables.pxToRem(19)};
+  color: ${variables.color.text};
+  font-size: ${clampBuilder(1542, 1920, 14, 16)};
+
+  @media ${variables.device.laptop} {
+    font-size: ${clampBuilder(1282, 1541, 14, 16)};
+  }
+  @media ${variables.device.tabletXL} {
+    font-size: ${clampBuilder(993, 1281, 12, 16)};
+  }
+  @media ${variables.device.tablet} {
+    font-size: ${clampBuilder(582, 992, 12, 16)};
+  }
+  @media ${variables.device.mobile} {
+    font-size: ${variables.pxToRem(14)};
+  }
+`
+const Label = styled.label`
+  input[type='radio'] {
+    /* Add if not using autoprefixer */
+    -webkit-appearance: none;
+    appearance: none;
+    /* For iOS < 15 to remove gradient background */
+    background-color: #fff;
+    /* Not removed via appearance */
+    margin: 0;
+    width: 25px;
+    height: 25px;
+    font: inherit;
+    box-shadow: 0px 10px 18px #0000001f;
+    border: 1px solid #0a0a0a;
+    border-radius: 50%;
+    display: grid;
+    place-content: center;
+    cursor: pointer;
+    &::before {
+      content: '';
+      width: 13px;
+      height: 13px;
+      border-radius: 50%;
+      transform: scale(0);
+      transition: 120ms transform ease-in-out;
+      background: #f7931e 0% 0% no-repeat padding-box;
+    }
+    & :checked::before {
+      transform: scale(1);
+    }
+  }
+`
+export const PositionContentWrapper = styled.div<{ positionLeft: boolean }>`
+  position: absolute;
+  top: 204px;
+  ${({ positionLeft }) => (positionLeft ? `left: unset` : `left:0`)};
+  ${({ positionLeft }) => (positionLeft ? `right: 0` : `right:unset`)};
+  width: ${variables.pxToRem(551)};
+  height: auto;
+
+  @media ${variables.device.tabletXL} {
+    width: ${clampBuilder(993, 1281, 524, 654)};
+  }
+  @media ${variables.device.tablet} {
+    ${({ positionLeft }) => (positionLeft ? `left: unset` : `left:0`)};
+    ${({ positionLeft }) => (positionLeft ? `right:0` : `right:unset`)};
+    width: ${variables.pxToRem(555)};
+  }
+  @media ${variables.device.mobile} {
+    top: 181px;
+    width: ${clampBuilder(360, 581, 330, 378)};
+    ${({ positionLeft }) => (positionLeft ? `left: unset` : `left:11px`)};
+    ${({ positionLeft }) => (positionLeft ? `right:11px` : `right:unset`)};
+  }
+`
+const ContentWrapper = styled.div<{ positionLeft: boolean; positionFirst: boolean }>`
+  position: relative;
+  box-shadow: 0px 0px 99px #00000017;
+  padding: ${variables.pxToRem(36)} ${variables.pxToRem(33)};
+  background: white;
+  color: ${variables.color.text};
+  transform: translate3d(0, 0, 0);
+
+  &::before {
+    content: '';
+    z-index: -1;
+    display: inline-block;
+    width: 1rem;
+    height: 1rem;
+    position: absolute;
+
+    ${({ positionLeft }) => (positionLeft ? `right: ${clampBuilder(1542, 1920, 26, 29)}` : `right:unset`)};
+    ${({ positionLeft }) => (positionLeft ? `left: unset` : `left:${clampBuilder(1542, 1920, 26, 29)}`)};
+    ${({ positionFirst }) => positionFirst && `left: ${clampBuilder(1542, 1920, 48, 52)}`};
+
+    top: -9px;
+    background: white;
+    transform: rotate(90deg);
+    clip-path: polygon(59% 0%, 0% 50%, 59% 100%);
+    box-shadow: 0px 0px 99px #00000017;
+  }
+  @media ${variables.device.laptop} {
+    & ::before {
+      ${({ positionLeft }) => (positionLeft ? `right:clamp(1.125rem, -1.9686rem + 3.8610vw, 1.75rem)` : `right:unset`)};
+      ${({ positionLeft }) => (positionLeft ? `left: unset` : `left:clamp(1.125rem, -1.9686rem + 3.8610vw, 1.75rem)`)};
+      ${({ positionFirst }) => positionFirst && `left: ${clampBuilder(1282, 1541, 48, 52)}`};
+    }
+  }
+  @media ${variables.device.tabletXL} {
+    & ::before {
+      ${({ positionLeft }) => (positionLeft ? `right:${clampBuilder(993, 1281, 25, 29)}` : `right:unset`)};
+      ${({ positionLeft }) => (positionLeft ? `left: unset` : `left:${clampBuilder(993, 1281, 25, 29)}`)};
+      ${({ positionFirst }) => positionFirst && `left: ${clampBuilder(993, 1281, 43, 53)}`};
+    }
+  }
+  @media ${variables.device.tablet} {
+    & ::before {
+      ${({ positionLeft }) =>
+        positionLeft ? `right:clamp(3.1875rem, -0.3613rem + 9.7561vw, 5.6875rem)` : `right:unset`};
+      ${({ positionLeft }) =>
+        positionLeft ? `left: unset` : `left:clamp(3.1875rem, -0.3613rem + 9.7561vw, 5.6875rem)`};
+      ${({ positionFirst }) => positionFirst && `left: ${clampBuilder(582, 992, 51, 92)}`};
+    }
+  }
+  @media ${variables.device.mobile} {
+    & ::before {
+      ${({ positionLeft }) => (positionLeft ? `right: ${clampBuilder(360, 581, 48, 89)}` : `right:unset`)};
+      ${({ positionLeft }) => (positionLeft ? `left: unset` : `left:${clampBuilder(360, 581, 48, 89)}`)};
+      ${({ positionFirst }) => positionFirst && `left: ${clampBuilder(360, 581, 47, 88)}`};
+    }
+  }
+`
+const ImageWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: ${variables.pxToRem(32)};
+  padding-bottom: ${variables.pxToRem(36)};
+  & div:not(.logo) {
+    overflow: hidden;
+    transition: all 0.3s ease 0s;
+    width: 107px;
+    height: 107px;
+    border: 1px solid rgb(211, 211, 211);
+    border-radius: 180px;
+    & img {
+      border-radius: 180px;
+    }
+  }
+  @media ${variables.device.tabletXL} {
+    gap: ${variables.pxToRem(24)};
+    padding-bottom: ${variables.pxToRem(23)};
+    flex-flow: row;
+    flex-wrap: wrap;
+    & div:not(.logo) {
+      width: 80px;
+      height: 80px;
+    }
+  }
 `
 
 export function StoryComponent() {
+  const [selectedTimeLine, setSelectedTimeLine] = useState(story[0].heading)
+  const handleChange = ({ target }) => {
+    const { value } = target
+    if (value) {
+      setSelectedTimeLine(value)
+    } else {
+      const value = target.getAttribute('data-value')
+      setSelectedTimeLine(value)
+    }
+  }
+
   return (
-    <Container className='tab-content content container'>
-      <TextRegular>
-        Bright Inventions is a software consulting studio based in Gdansk, Poland. Since 2012 we have built software for
-        more than 40 businesses worldwide. Our expertise in mobile, web, blockchain and IoT systems has been highly
-        appreciated by our clients from UK, Germany, Netherlands, Norway Israel and more.
-      </TextRegular>
-      <Timeline>
-        {story.map(item => {
-          return (
-            <Timeline.Element position={item.position.toLowerCase()}>
-              <Timeline.Heading>
-                {item.heading}
-                {item.images?.map(image => (
-                  <Timeline.Image key={image.src} {...image} />
-                ))}
-                {item.logos?.map(image => (
-                  <Timeline.Logo key={image.src} {...image} />
-                ))}
-              </Timeline.Heading>
-              <Timeline.Subheading>{item.subheading}</Timeline.Subheading>
-              <Timeline.Content>
-                <StoryTextRegular>{item.content}</StoryTextRegular>
-              </Timeline.Content>
-            </Timeline.Element>
-          )
-        })}
-      </Timeline>
-    </Container>
+    <CustomSection
+      paddingProps='8.125rem 0 25rem'
+      paddingLaptop='6.25rem 0 25rem'
+      paddingTabletXL='4.375rem 0 0'
+      paddingTablet='7.6875rem 0 0'
+      paddingMobileProps='4.5625rem 0 0'
+    >
+      <StoriesWrapperScroll>
+        <StoriesWrapperPosition>
+          <StoriesWrapper>
+            {story.map((item, index) => {
+              const isChecked = selectedTimeLine === item.heading
+              const isLeft = index >= 5
+              const isFirst = index <= 1
+
+              return (
+                <StoryWrapper key={item.heading}>
+                  <Data
+                    className={isChecked ? 'is-active' : ''}
+                    data-value={item.heading}
+                    onMouseOver={handleChange}
+                    onChange={handleChange}
+                  >
+                    {item.heading}
+                  </Data>
+                  <Label>
+                    <input
+                      type='radio'
+                      value={item.heading}
+                      checked={isChecked}
+                      onMouseOver={handleChange}
+                      onChange={handleChange}
+                    />
+                  </Label>
+                  {isChecked && (
+                    <PositionContentWrapper positionLeft={isLeft}>
+                      <ContentWrapper positionLeft={isLeft} positionFirst={isFirst}>
+                        <ImageWrapper>
+                          {item.images?.map(image => (
+                            <Timeline.Image key={image.src} {...image} />
+                          ))}
+                          {item.logos?.map(image => (
+                            <Timeline.Logo key={image.src} {...image} />
+                          ))}
+                        </ImageWrapper>
+
+                        <TextRegular>{item.content}</TextRegular>
+                      </ContentWrapper>
+                    </PositionContentWrapper>
+                  )}
+                </StoryWrapper>
+              )
+            })}
+          </StoriesWrapper>
+        </StoriesWrapperPosition>
+      </StoriesWrapperScroll>
+    </CustomSection>
   )
 }
