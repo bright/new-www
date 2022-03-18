f03c4a93de55f94602fd2be88ca8be9b33a10b7b
codersbros <p.grden@codersbros.pl>
2022-03-18 16:34:15 +0100

    create StandWithUkraine.icon in components/icons

---
+++ src/components/icons/StandWithUkraine.icon.tsx
@@ -0,0 +1,175 @@
+import { Link } from 'gatsby'
+import React from 'react'
+import styled from 'styled-components'
+import variables from '../../styles/variables'
+
+const Svg = styled.svg`
+  @media ${variables.device.laptop} {
+    width: ${variables.pxToRem(314)};
+    height: auto;
+  }
+  @media ${variables.device.tabletXL} {
+    width: ${variables.pxToRem(268)};
+    height: auto;
+  }
+  @media ${variables.device.tablet} {
+    width: ${variables.pxToRem(267)};
+    height: auto;
+  }
+  @media ${variables.device.mobile} {
+    width: ${variables.pxToRem(200)};
+    height: auto;
+  }
+`
+export function StandWithUkraine() {
+  return (
+    <Link to='blog/stand-with-ukraine-how-you-can-help/'>
+      <Svg xmlns='http://www.w3.org/2000/svg' width='359.779' height='73.94' viewBox='0 0 359.779 73.94'>
+        <g id='StandWithUkraine' data-name='StandWithUkraine' transform='translate(-0.991 6.17)'>
+          <text
+            id='_'
+            data-name='#'
+            transform='translate(88.51 23.83)'
+            font-size='30.78'
+            font-family='Lato'
+            font-weight='700'
+          >
+            <tspan x='0' y='0'>
+              #
+            </tspan>
+          </text>
+          <text id='S' transform='translate(106.36 23.83)' font-size='30.78' font-family='Lato' font-weight='700'>
+            <tspan x='0' y='0'>
+              S
+            </tspan>
+          </text>
+          <text id='tan' transform='translate(122.57 23.83)' font-size='30.78' font-family='Lato' font-weight='700'>
+            <tspan x='0' y='0'>
+              tan
+            </tspan>
+          </text>
+          <text id='d' transform='translate(167 23.83)' font-size='30.78' font-family='Lato' font-weight='700'>
+            <tspan x='0' y='0'>
+              d
+            </tspan>
+          </text>
+          <text id='W' transform='translate(184.25 23.83)' font-size='30.78' font-family='Lato' font-weight='700'>
+            <tspan x='0' y='0'>
+              W
+            </tspan>
+          </text>
+          <text id='it' transform='translate(216.45 23.83)' font-size='30.78' font-family='Lato' font-weight='700'>
+            <tspan x='0' y='0'>
+              it
+            </tspan>
+          </text>
+          <text id='h' transform='translate(235.71 23.83)' font-size='30.78' font-family='Lato' font-weight='700'>
+            <tspan x='0' y='0'>
+              h
+            </tspan>
+          </text>
+          <text id='U' transform='translate(252.44 23.83)' font-size='30.78' font-family='Lato' font-weight='700'>
+            <tspan x='0' y='0'>
+              U
+            </tspan>
+          </text>
+          <text id='k' transform='translate(275.04 23.83)' font-size='30.78' font-family='Lato' font-weight='700'>
+            <tspan x='0' y='0'>
+              k
+            </tspan>
+          </text>
+          <text id='r' transform='translate(291.49 23.83)' font-size='30.78' font-family='Lato' font-weight='700'>
+            <tspan x='0' y='0'>
+              r
+            </tspan>
+          </text>
+          <text id='aine' transform='translate(302.77 23.83)' font-size='30.78' font-family='Lato' font-weight='700'>
+            <tspan x='0' y='0'>
+              aine
+            </tspan>
+          </text>
+          <text
+            id='_2'
+            data-name='#'
+            transform='translate(88.51 60.77)'
+            font-size='30.78'
+            font-family='Lato'
+            font-weight='700'
+          >
+            <tspan x='0' y='0'>
+              #
+            </tspan>
+          </text>
+          <text
+            id='S-2'
+            data-name='S'
+            transform='translate(106.36 60.77)'
+            font-size='30.78'
+            font-family='Lato'
+            font-weight='700'
+          >
+            <tspan x='0' y='0'>
+              S
+            </tspan>
+          </text>
+          <text id='t' transform='translate(122.57 60.77)' font-size='30.78' font-family='Lato' font-weight='700'>
+            <tspan x='0' y='0'>
+              t
+            </tspan>
+          </text>
+          <text id='o' transform='translate(133.89 60.77)' font-size='30.78' font-family='Lato' font-weight='700'>
+            <tspan x='0' y='0'>
+              o
+            </tspan>
+          </text>
+          <text id='p' transform='translate(151.58 60.77)' font-size='30.78' font-family='Lato' font-weight='700'>
+            <tspan x='0' y='0'>
+              p
+            </tspan>
+          </text>
+          <text
+            id='W-2'
+            data-name='W'
+            transform='translate(167.29 60.77)'
+            font-size='30.78'
+            font-family='Lato'
+            font-weight='700'
+          >
+            <tspan x='0' y='0'>
+              W
+            </tspan>
+          </text>
+          <text id='a' transform='translate(197.75 60.77)' font-size='30.78' font-family='Lato' font-weight='700'>
+            <tspan x='0' y='0'>
+              a
+            </tspan>
+          </text>
+          <text
+            id='r-2'
+            data-name='r'
+            transform='translate(213.37 60.77)'
+            font-size='30.78'
+            font-family='Lato'
+            font-weight='700'
+          >
+            <tspan x='0' y='0'>
+              r
+            </tspan>
+          </text>
+          <path
+            id='Path_36390'
+            data-name='Path 36390'
+            d='M75.3,22.2A18.7,18.7,0,0,0,56.3,4,18.493,18.493,0,0,0,38.2,22.1,18.6,18.6,0,1,0,1,22.2,26.578,26.578,0,0,0,3,33H73.2A26.665,26.665,0,0,0,75.3,22.2Z'
+            fill='#0947af'
+          />
+          <path
+            id='Path_36391'
+            data-name='Path 36391'
+            d='M38.1,63s23-15.5,29.7-22.3A24.383,24.383,0,0,0,73.2,33H3a24.383,24.383,0,0,0,5.4,7.7C15.2,47.5,38.1,63,38.1,63Z'
+            fill='#fdcc00'
+          />
+        </g>
+      </Svg>
+    </Link>
+  )
+}
