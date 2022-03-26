da29d7e670b7a48ea3e03846071e9e3359fa27ea
codersbros <p.grden@codersbros.pl>
2022-03-26 20:55:38 +0100

    add loadable function to import component Ratings in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
-
+import loadable from '@loadable/component'
 import { Page } from '../layout/Page'
 import { Header } from '../components/home/Header'
 import { Achievements } from '../components/home/Achievements'
 import { TechnologyTags } from '../components/shared/TechnologyTags'
 import { OurServices } from '../components/home/OurServices'
 import { Projects } from '../components/home/Projects'
-import Ratings from '../components/shared/Ratings'
+// import Ratings from '../components/shared/Ratings'
+const Ratings = loadable(() => import('../components/shared/Ratings'))
 import { Contact } from '../components/shared/Contact'
 import HeroHeaderImages from '../components/home/HeroHeaderImages'
 import { PopularBlogPosts } from '../components/home/PopularBlogPosts'
