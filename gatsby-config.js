dec584315b4b0cc3fba2d76f457ce7070c2442cf
codersbros <p.grden@codersbros.pl>
2022-03-30 13:44:19 +0200

    add options to gatsby-plugin-purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -116,7 +116,7 @@ module.exports = {
         develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+        purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
         purgeCSSOptions: {
           // https://purgecss.com/configuration.html#options
           // safelist: ['safelist'], // Don't remove this selector
