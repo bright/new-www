9a453bca7b6590b74beea18b3f602b64050e1423
codersbros <p.grden@codersbros.pl>
2022-03-30 13:21:35 +0200

    add  gatsby-plugin-purgecss to gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -109,6 +109,21 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
+{
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        develop: true, // Enable while using `gatsby develop`
+        // tailwind: true, // Enable tailwindcss support
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+        purgeCSSOptions: {
+          // https://purgecss.com/configuration.html#options
+          // safelist: ['safelist'], // Don't remove this selector
+        },
+        // More options defined here https://purgecss.com/configuration.html#options
+      },
+    },
 
     {
       resolve: `gatsby-transformer-remark`,
