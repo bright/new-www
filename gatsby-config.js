4d4815fa40239142ed169e402ad26baf4d234051
codersbros <p.grden@codersbros.pl>
2022-03-30 23:41:39 +0200

    change options gatsby-plugin-purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -115,7 +115,7 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
-        ignore: ['/carousel.min.css'], // Ignore files/folders
+        // ignore: ['/carousel.min.css'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
         purgeCSSOptions: {
           // https://purgecss.com/configuration.html#options
