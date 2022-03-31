4f7ec475c841051ebaf3ba31ed4be82155cd79a5
codersbros <p.grden@codersbros.pl>
2022-03-31 23:35:44 +0200

    add ArrowBack.icon component to components\icons

---
+++ src/components/icons/ArrowBack.icon.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+import variables from '../../styles/variables'
+
+const Svg = styled.svg`
+  transform: rotate(270deg);
+`
+export function ArrowBack() {
+  return (
+    <Svg xmlns='http://www.w3.org/2000/svg' width='18.748' height='26.799' viewBox='0 0 18.748 26.799'>
+      <g id='strzalka' transform='translate(16.635 2.113) rotate(90)'>
+        <path
+          id='Path_35449'
+          data-name='Path 35449'
+          d='M8.617,0,0,7.238H23.186'
+          transform='translate(0 0)'
+          fill='none'
+          stroke='#520993'
+          stroke-linecap='round'
+          stroke-linejoin='round'
+          stroke-width='3'
+        />
+        <line
+          id='Line_39'
+          data-name='Line 39'
+          x2='8.617'
+          y2='7.238'
+          transform='translate(0 7.283)'
+          fill='none'
+          stroke='#520993'
+          stroke-linecap='round'
+          stroke-width='3'
+        />
+      </g>
+    </Svg>
+  )
+}
