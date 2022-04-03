2485ac90e8804a7adc8c6907ed8b34349b82e441
codersbros <p.grden@codersbros.pl>
2022-04-03 17:41:30 +0200

    use use-OnScreen hook in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import loadable from '@loadable/component'
 import { Page } from '../layout/Page'
 import { Header } from '../components/home/Header'
@@ -13,7 +13,11 @@ const PopularBlogPosts = loadable(() => import('../components/home/PopularBlogPo
 import '../styles/_page-index.scss'
 import { BlockchainExperts } from '../components/home/BlockchainExperts'
 import { GlobalStyle } from '../styles/global'
+import useOnScreen from '../components/utils/use-onscreen'
+
 export default () => {
+  const ref: any = useRef<HTMLDivElement>()
+  const onScreen: boolean = useOnScreen<HTMLDivElement>(ref, '400px')
   return (
     <Page className='page-index'>
       <GlobalStyle />
@@ -26,7 +30,13 @@ export default () => {
       <Projects />
       <Ratings />
       <PopularBlogPosts />
-      <Contact formButton='Business Contact Form Button' actionFormButton='Click Submit Business Form' />
+      <div ref={ref}>
+        {onScreen ? (
+          <Contact formButton='Business Contact Form Button' actionFormButton='Click Submit Business Form' />
+        ) : (
+          <div style={{ height: '400px' }}></div>
+        )}
+      </div>
     </Page>
   )
 }
