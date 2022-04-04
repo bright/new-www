0556f7d5b814c104757474270742d3948ba6e2c2
codersbros <p.grden@codersbros.pl>
2022-04-04 20:45:46 +0200

    create dynamic link preload to load fonts

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,8 +5,9 @@
  */
 
 // You can delete this file if you're not using it
-import React from "react"
+const React = require("react")
 import { GlobalStyle } from "./src/styles/global"
+
 
 export const wrapPageElement = ({ element }) => (
   <>
@@ -14,3 +15,43 @@ export const wrapPageElement = ({ element }) => (
     {element}
   </>
 )
+const fs = require('fs')
+export const onRenderBody = ({ setHeadComponents }) => {
+
+  const files = getFilesFromPath("./public/static", ".woff2")
+  const preload = [
+      'montserrat-v23-latin-regular',
+      'montserrat-v23-latin-600',
+      'montserrat-v23-latin-700',
+      'montserrat-v23-latin-800',
+      'montserrat-v23-latin-ext_latin-900',
+      'lato-v22-latin-ext_latin-regular',
+      'lato-v22-latin-700',
+      'lato-v22-latin-900'
+    ]
+
+  setHeadComponents([
+    files.map((file, i) => {
+      return preload.map((font, key) => {
+        const fileBeginning = file.split('-').slice(0,-1).join('-')
+        if (fileBeginning === font) {
+          return(
+            <link
+      				key={key}
+      				rel='preload'
+      				as='font'
+      				type='font/woff2'
+      				crossOrigin='anonymous'
+      				href={`/static/${file}`}
+      			/>
+          )
+        }
+      })
+    })
+  ])
+}
+
+function getFilesFromPath(path, extension) {
+  let dir = fs.readdirSync( path );
+  return dir.filter( elm => elm.match(new RegExp(`.*\.(${extension})`, 'ig')));
+}
