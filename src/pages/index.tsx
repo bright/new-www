84949175360580e5b2d193856f5a5dfe92815b2d
codersbros <p.grden@codersbros.pl>
2022-04-06 13:27:51 +0200

    render hero header images by react suspense to reduce first contentful paint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,19 +8,26 @@ import { OurServices } from '../components/home/OurServices'
 import { Projects } from '../components/home/Projects'
 const Ratings = loadable(() => import('../components/shared/Ratings'))
 import { Contact } from '../components/shared/Contact'
-const HeroHeaderImages = loadable(() => import('../components/home/HeroHeaderImages'))
+// const HeroHeaderImages = loadable(() => import('../components/home/HeroHeaderImages'))
 const PopularBlogPosts = loadable(() => import('../components/home/PopularBlogPosts'))
 import '../styles/_page-index.scss'
 import { BlockchainExperts } from '../components/home/BlockchainExperts'
 import { GlobalStyle } from '../styles/global'
+const HeroHeaderImages = React.lazy(() => import('../components/home/HeroHeaderImages'))
 
 export default () => {
+  const isSSR = typeof window === 'undefined'
+
   return (
     <Page className='page-index'>
       <GlobalStyle />
       <Header />
       <Achievements />
-      <HeroHeaderImages />
+      {!isSSR && (
+        <React.Suspense fallback={<div />}>
+          <HeroHeaderImages />
+        </React.Suspense>
+      )}
       <TechnologyTags />
       <OurServices />
       <BlockchainExperts />
