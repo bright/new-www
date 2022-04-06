a82d199f8efed0bea1ea73101d17f67245a76d37
codersbros <p.grden@codersbros.pl>
2022-04-06 13:29:42 +0200

    remove unuse ref

--- src/components/home/BlockchainExperts.tsx
+++ src/components/home/BlockchainExperts.tsx
@@ -60,7 +60,7 @@ export const BlockchainExperts = () => {
     <CustomSection>
       <CustomContainer>
         <CustomSectionTitle>we are blockchain experts</CustomSectionTitle>
-        <CustomSectionInner ref={ref}>
+        <CustomSectionInner>
           <BlockchainExpertsTextRegural>
             We have experience in developing blockchain projects in Ethereum and Substrate. We believe that blockchain
             is the technology of the future that’s why we’ve developed our original project for blockchain enthusiasts
