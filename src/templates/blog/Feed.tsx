85667f0e178a5d0cc27f5bdbafbeaa5e8cfd3355
codersbros <p.grden@codersbros.pl>
2022-04-06 21:20:51 +0200

    wrap blog feed in link

--- src/templates/blog/Feed.tsx
+++ src/templates/blog/Feed.tsx
@@ -8,6 +8,7 @@ import * as styles from './Feed.module.scss'
 import { GatsbyImage, getImage, getSrc } from 'gatsby-plugin-image'
 import styled from 'styled-components'
 import variables from '../../styles/variables'
+import { Link } from 'gatsby'
 
 const BlogPostDummyUrl = '/images/dummy/blog_post.png'
 
@@ -42,36 +43,35 @@ export const BlogFeed = ({ posts, numToSliced }: Props) => {
 }
 
 const Post: React.FC<{ post: BlogPostModel }> = ({ post }) => {
-  const redirect = () => {
-    window.location.href = deleteTimestampFromUrl(post.slug)
-  }
   return (
-    <div className={styles.entry} onClick={redirect}>
-      <ImageWrapper className={styles.image}>
-        <GatsbyImage
-          image={getImage(post.image)!}
-          alt={post.title}
-          className='post-img'
-          imgStyle={{ objectFit: 'cover' }}
-        />
-      </ImageWrapper>
-      <div className={styles.content}>
-        <div className={styles.postInfo}>
-          <div className={styles.date}>
-            {formatDate(
-              new Date(
-                post.date
-                  .replace(/-/g, '/')
-                  .replace('T', ' ')
-                  .replace(/\..*|\+.*/, '')
-              ),
-              'MMM dd, yyyy'
-            )}
+    <div className={styles.entry}>
+      <Link to={deleteTimestampFromUrl(post.slug)}>
+        <ImageWrapper className={styles.image}>
+          <GatsbyImage
+            image={getImage(post.image)!}
+            alt={post.title}
+            className='post-img'
+            imgStyle={{ objectFit: 'cover' }}
+          />
+        </ImageWrapper>
+        <div className={styles.content}>
+          <div className={styles.postInfo}>
+            <div className={styles.date}>
+              {formatDate(
+                new Date(
+                  post.date
+                    .replace(/-/g, '/')
+                    .replace('T', ' ')
+                    .replace(/\..*|\+.*/, '')
+                ),
+                'MMM dd, yyyy'
+              )}
+            </div>
+            <div className={styles.tags}>{post.tags.join(', ')}</div>
           </div>
-          <div className={styles.tags}>{post.tags.join(', ')}</div>
+          <div className={styles.title}>{post.title}</div>
         </div>
-        <div className={styles.title}>{post.title}</div>
-      </div>
+      </Link>
     </div>
   )
 }
