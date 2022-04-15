256a22d70336f38a4e6a672236df6338124854f6
Piotr Mionskowski <piotr.mionskowski@brightinventions.pl>
2022-04-15 12:43:54 +0200

    Remove unsued bulma code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,12 +5,11 @@
  */
 
 // You can delete this file if you're not using it
-const React = require("react")
+import React from 'react'
 import { GlobalStyle } from './src/styles/global'
 import {
   registerGlobalMailtoClickHandler,
 } from './src/report-mailto-click-to-google-analytics'
-
 
 export const wrapPageElement = ({ element }) => (
   <>
@@ -22,15 +21,4 @@ export const wrapPageElement = ({ element }) => (
 export const onInitialClientRender = () => {
   registerGlobalMailtoClickHandler();
 }
-
-const loadableReady = require("@loadable/component").loadableReady
-
-export const replaceHydrateFunction = () => {
-  return (element, container, callback) => {
-    loadableReady(() => {
-      ReactDOM.render(element, container, callback)
-    })
-  }
-}
-
 
