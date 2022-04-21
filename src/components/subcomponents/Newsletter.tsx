9cc6b670a43a2e043b31fffb9d96b90f2ec7a42d
codersbros <p.grden@codersbros.pl>
2022-04-21 12:25:39 +0200

    apply script get response

--- src/components/subcomponents/Newsletter.tsx
+++ src/components/subcomponents/Newsletter.tsx
@@ -71,46 +71,14 @@ const Button = styled.button`
 export default function Newsletter() {
   return (
     <NewsletterWrapper>
-      <StaticImage src='../../../static/images/newsletter.png' alt='Newsletter' className='newsimage' />
-      {/* <script
-        type='text/javascript'
-        src='https://app.getresponse.com/view_webform_v2.js?u=QX16N&webforms_id=hiz1B'
-        data-webform-id='hiz1B'
-        defer={true}
-      ></script>
-      <div id='hiz1B'></div> */}
       <FormWrapper>
-        <CustomSectionTitle
-          margin='0 0 20px 0'
-          laptopMargin='0 0 20px 0'
-          tabletXLMargin='0 0 20px 0'
-          tabletMargin='116px 0 20px 0'
-          mobileMargin='82px 0 20px 0'
-        >
-          join our <span> bright</span> newsletter
-        </CustomSectionTitle>
-        <CustomTextRegular>
-          Sing up to recive <b> once a month</b> new blog posts, info about free workshop and more. SPAM shall not pass!
-        </CustomTextRegular>
-        <Form action='https://app.getresponse.com/add_subscriber.html' accept-charset='utf-8' method='post'>
-          <SubscribeWrapper>
-            <input type='text' name='first_name' placeholder='Name' />
-            <input type='text' name='email' placeholder='Email' />
-          </SubscribeWrapper>
-
-          <WrapperAgreeNewslatter>
-            <input id='webform_consent#hbNh_0' type='checkbox' name='webform[consent#hbNh-ver#GLN5]' value='true' />{' '}
-            <label htmlFor='webform_consent#hbNh_0'>
-              {' '}
-              <span>I agree to receive communication from Bright Inventions.</span>
-            </label>
-          </WrapperAgreeNewslatter>
-
-          <input type='hidden' name='campaign_token' value='zjdq9' />
-          <Button type='submit' value='Subscribe'>
-            <CustomTextRegular>I wont to join</CustomTextRegular>
-          </Button>
-        </Form>
+        <StaticImage src='../../../static/images/newsletter.png' alt='Newsletter' className='newsimage' />
+        <script
+          type='text/javascript'
+          src='https://app.getresponse.com/view_webform_v2.js?u=QX16N&webforms_id=hiz1B'
+          data-webform-id='hiz1B'
+          defer={true}
+        ></script>
       </FormWrapper>
     </NewsletterWrapper>
   )
