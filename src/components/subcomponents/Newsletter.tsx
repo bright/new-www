f3ec52bc3b90adbc9e1cef5f0add1d624fc25b85
codersbros <p.grden@codersbros.pl>
2022-04-22 12:33:15 +0200

    place get response web form in iframe

--- src/components/subcomponents/Newsletter.tsx
+++ src/components/subcomponents/Newsletter.tsx
@@ -2,13 +2,17 @@ import { StaticImage } from 'gatsby-plugin-image'
 import React from 'react'
 import styled from 'styled-components'
 import variables from '../../styles/variables'
-import { CustomSectionTitle, CustomTextRegular } from '../shared'
 
 const NewsletterWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 0 0 ${variables.pxToRem(64)} 0;
+  overflow: hidden;
+  margin: 0 auto;
+  & .newsimage {
+    flex-basis: 40%;
+  }
 
   @media ${variables.device.tablet} {
     flex-direction: column;
@@ -16,8 +20,28 @@ const NewsletterWrapper = styled.div`
   }
 `
 const FormWrapper = styled.div`
-  max-width: ${variables.pxToRem(460)};
+  min-width: ${variables.pxToRem(550)};
   padding: 0 ${variables.pxToRem(30)};
+  overflow: hidden;
+
+  div.iframe-wrapper {
+    width: 460px;
+    height: 531px;
+    top: 0px;
+    left: 0px;
+    margin: auto;
+
+    @media ${'screen and (max-width: 460px)'} {
+      width: 320px;
+    }
+
+    & iframe {
+      height: ${variables.pxToRem(600)};
+      width: 100%;
+      margin: 0 auto;
+      overflow: hidden;
+    }
+  }
 `
 const Form = styled.form`
   display: flex;
@@ -36,49 +60,17 @@ const WrapperAgreeNewslatter = styled.div`
     font-size: ${variables.pxToRem(11)};
   }
 `
-const SubscribeWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: ${variables.pxToRem(20)} ${variables.pxToRem(10)};
-  & input {
-    margin-top: ${variables.pxToRem(10)};
-    width: 100%;
-    height: ${variables.pxToRem(50)};
-    border: 2px solid ${variables.color.black};
-    padding: ${variables.pxToRem(10)};
-    & ::placeholder {
-      font-weight: 600;
-      color: ${variables.color.text};
-    }
-  }
-`
-const Button = styled.button`
-  background-color: ${variables.color.primary};
-  color: #ffff;
-  height: ${variables.pxToRem(50)};
-  width: 100%;
-  max-width: ${variables.pxToRem(200)};
-  border: none;
-  font-size: ${variables.pxToRem(20)};
-  font-weight: 700;
-  margin: 0 auto;
-  margin-top: ${variables.pxToRem(30)};
-  cursor: pointer;
-  & :hover {
-    background-color: ${variables.color.black};
-  }
-`
 export default function Newsletter() {
   return (
     <NewsletterWrapper>
+      <StaticImage src='../../../static/images/newsletter.png' alt='Newsletter' className='newsimage' />
       <FormWrapper>
-        <StaticImage src='../../../static/images/newsletter.png' alt='Newsletter' className='newsimage' />
-        <script
-          type='text/javascript'
-          src='https://app.getresponse.com/view_webform_v2.js?u=QX16N&webforms_id=hiz1B'
-          data-webform-id='hiz1B'
-          defer={true}
-        ></script>
+        <div className='iframe-wrapper'>
+          <iframe
+            className='responsive-iframe'
+            src='https://app.getresponse.com/site2/5d4d6f8b6908199482efeb84d0edf9a5/?u=QX16N&webforms_id=hiz1B'
+          ></iframe>
+        </div>
       </FormWrapper>
     </NewsletterWrapper>
   )
