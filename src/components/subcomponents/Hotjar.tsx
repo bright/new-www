b58aff072add9af7d8787ac97e4a7120b6d48fb3
codersbros <p.grden@codersbros.pl>
2022-06-06 08:56:59 +0200

    remove hotjar async load

--- src/components/subcomponents/Hotjar.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { useScriptHotjar } from '../utils/use-script-hotjar'
-
-export default function Hotjar() {
-  const status: string = useScriptHotjar('https://static.hotjar.com/c/hotjar-2864857.js?sv=6')
-  return <>{status === 'ready'}</>
-}
--- src/layout/Page.tsx
+++ src/layout/Page.tsx
@@ -11,7 +11,6 @@ import Helmet from 'react-helmet'
 import { fbShareImage } from '../meta/bright-logo'
 import { resolveUrl } from '../meta/resolve-url'
 import { useLocation } from '@reach/router'
-import HotjarWrapper from '../components/subcomponents/HotjarWrapper'
 
 export const Page: React.FC<{ className?: string }> = ({ children, className }) => {
   const [mobileMenuOpened, setMobileMenuOpened] = useState(false)
@@ -35,7 +34,7 @@ export const Page: React.FC<{ className?: string }> = ({ children, className }) 
         <script type='text/javascript' src={'https://widget.clutch.co/static/js/widget.js'} defer={true} />
       </Helmet>
       <TopNavigation path={pathname} toggled={setMobileMenuOpened} />
-      <HotjarWrapper />
+
       {children}
       <Footer />
     </div>
