4b352b5cb17f3bcf46f0aacd757f75f09d9fbc59
codersbros <p.grden@codersbros.pl>
2022-06-08 23:29:05 +0200

    change layout _Traits page\carrer

--- src/pages/career/_Traits.tsx
+++ src/pages/career/_Traits.tsx
@@ -9,8 +9,15 @@ import {
   TextRegular,
   CustomSectionTitle,
   CustomSectionInner,
+  FlexWrapper,
+  CustomTextTitle,
 } from '../../components/shared'
 import variables from '../../styles/variables'
+import ResponsibilityIcon from '../../assets/responsibility.svg'
+import FlexibilityIcon from '../../assets/flexibility.svg'
+import TeamworkIcon from '../../assets/teamwork.svg'
+import PositiveAttitudeIcon from '../../assets/positive_Attitude.svg'
+import ClientOrientationIcon from '../../assets/client-orientation.svg'
 // import { TextRegular } from './../../components/shared/index'
 
 const SectionTitleEx = styled(CustomSectionTitle)`
@@ -29,25 +36,23 @@ const SectionTitleEx = styled(CustomSectionTitle)`
   }
 `
 
-const TextTitle = styled.h3`
-  margin: 4rem 0 2.25rem;
-  font-weight: 700;
-  font-size: 1.75rem;
-  line-height: 2.125rem;
-  text-align: center;
+const TextTitle = styled(CustomTextTitle)`
+  margin: 0 0 36px;
+  text-align: left;
+  text-transform: lowercase;
   @media ${variables.device.laptop} {
     font-size: 1.5625rem;
-    margin: 4rem 0 2rem;
+  }
+
+  @media ${variables.device.tablet} {
+    text-align: center;
   }
 
   @media ${variables.device.mobile} {
     font-size: 1.125rem;
-    margin: 4rem 0 1.5rem;
-    text-align: left;
   }
 `
 const TextRegularTraits = styled(TextRegular)`
-  font-size: 1.375rem;
   color: ${variables.color.text};
 
   @media ${variables.device.laptop} {
@@ -63,6 +68,25 @@ const TextRegularTraits = styled(TextRegular)`
     }
   }
 `
+const TraitsFigure = styled.figure`
+  & svg {
+    width: 142px;
+    margin-top: ${variables.pxToRem(48)};
+  }
+  @media ${variables.device.tablet} {
+    margin: 0 auto;
+    & svg {
+      width: 48px;
+      margin-top: ${variables.pxToRem(0)};
+    }
+  }
+`
+const Wrapper = styled.div`
+  margin-top: ${variables.pxToRem(64)};
+  @media ${variables.device.tablet} {
+    margin-top: ${variables.pxToRem(83)};
+  }
+`
 
 const Traits: React.FC = () => {
   return (
@@ -74,40 +98,86 @@ const Traits: React.FC = () => {
             We believe that bright ideas and bright products come out of bright minds. We work together making the most
             of experience and expertise we have.
           </TextRegularTraits>
-
-          <TextTitle>Teamwork</TextTitle>
-          <TextRegularTraits>
-            It is only through teamwork that we achieve brightness. We know each others’ strengths and we also can ask
-            for help when we need. We work together to bring our passions and expertise to make Bright Inventions the
-            best place it can be.
-          </TextRegularTraits>
-
-          <TextTitle>Responsibility</TextTitle>
-          <TextRegularTraits>
-            We support and share responsibility with the members of our team. We also actively take responsibility for
-            our own self-development. Everything is everyone’s responsibility and we own our collective results
-            together.
-          </TextRegularTraits>
-
-          <TextTitle>Positive attitude</TextTitle>
-          <TextRegularTraits>
-            We love what we do and the people around us. In fact, we believe that happy people are more motivated,
-            efficient and creative, so we bring a positive attitude to everything we do.
-          </TextRegularTraits>
-
-          <TextTitle>Flexibility</TextTitle>
-          <TextRegularTraits>
-            We balance ambition with flexibility because what matters is the final result. We are nimble. We evolve and
-            adapt. People who work here are not afraid of taking risk and making mistakes as long as we learn from them.
-            We encourage the diversity of thought.
-          </TextRegularTraits>
-
-          <TextTitle>Client orientation</TextTitle>
-          <TextRegularTraits>
-            We are only successful if our clients are - and that drives everything we do. Our team will overcome
-            obstacles, find solutions and deliver results on time. Every action we take enables our clients to make a
-            bigger impact on the world.
-          </TextRegularTraits>
+          <Wrapper>
+            <FlexWrapper
+              desktopGap='34px'
+              laptopGap='70px'
+              tabletDirection='column'
+              tabletGap='27px'
+              tabletContent='center'
+            >
+              <TraitsFigure>
+                <ResponsibilityIcon />
+              </TraitsFigure>
+              <div>
+                <TextTitle>Responsibility</TextTitle>
+                <TextRegularTraits>
+                  We support and share responsibility with the members of our team. We also actively take responsibility
+                  for our own self-development. Everything is everyone’s responsibility and we own our collective
+                  results together.
+                </TextRegularTraits>
+              </div>
+            </FlexWrapper>
+          </Wrapper>
+          <Wrapper>
+            <FlexWrapper desktopGap='34px' laptopGap='70px' tabletDirection='column' tabletGap='27px'>
+              <TraitsFigure>
+                <FlexibilityIcon />
+              </TraitsFigure>
+              <div>
+                <TextTitle>Flexibility</TextTitle>
+                <TextRegularTraits>
+                  We balance ambition with flexibility because what matters is the final result. We are nimble. We
+                  evolve and adapt. People who work here are not afraid of taking risk and making mistakes as long as we
+                  learn from them. We encourage the diversity of thought.
+                </TextRegularTraits>
+              </div>
+            </FlexWrapper>
+          </Wrapper>
+          <Wrapper>
+            <FlexWrapper desktopGap='34px' laptopGap='70px' tabletDirection='column' tabletGap='27px'>
+              <TraitsFigure>
+                <TeamworkIcon />
+              </TraitsFigure>
+              <div>
+                <TextTitle>Teamwork</TextTitle>
+                <TextRegularTraits>
+                  It is only through teamwork that we achieve brightness. We know each others’ strengths and we also can
+                  ask for help when we need. We work together to bring our passions and expertise to make Bright
+                  Inventions the best place it can be.
+                </TextRegularTraits>
+              </div>
+            </FlexWrapper>
+          </Wrapper>
+          <Wrapper>
+            <FlexWrapper desktopGap='34px' laptopGap='70px' tabletDirection='column' tabletGap='27px'>
+              <TraitsFigure>
+                <PositiveAttitudeIcon />
+              </TraitsFigure>
+              <div>
+                <TextTitle>Positive attitude</TextTitle>
+                <TextRegularTraits>
+                  We love what we do and the people around us. In fact, we believe that happy people are more motivated,
+                  efficient and creative, so we bring a positive attitude to everything we do.
+                </TextRegularTraits>
+              </div>
+            </FlexWrapper>
+          </Wrapper>
+          <Wrapper>
+            <FlexWrapper desktopGap='34px' laptopGap='70px' tabletDirection='column' tabletGap='27px'>
+              <TraitsFigure>
+                <ClientOrientationIcon />
+              </TraitsFigure>
+              <div>
+                <TextTitle>Client orientation</TextTitle>
+                <TextRegularTraits>
+                  We are only successful if our clients are - and that drives everything we do. Our team will overcome
+                  obstacles, find solutions and deliver results on time. Every action we take enables our clients to
+                  make a bigger impact on the world.
+                </TextRegularTraits>
+              </div>
+            </FlexWrapper>
+          </Wrapper>
         </CustomSectionInner>
       </CustomSection>
     </div>
