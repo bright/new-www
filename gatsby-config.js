2e97bb84f1443881b5175f509eef239bb3d49ca7
codersbros <p.grden@codersbros.pl>
2022-06-20 22:22:28 +0200

    add swiper.min.css and pagination.min.css to ignore options gatsby-plugin-purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -122,7 +122,13 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
-        ignore: ['/carousel.min.css', 'prismjs/', 'style/bulma/sass/components/modal.sass'], // Ignore files/folders
+        ignore: [
+          '/carousel.min.css',
+          'prismjs/',
+          'style/bulma/sass/components/modal.sass',
+          '/swiper.min.css',
+          '/pagination.min.css',
+        ], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
         purgeCSSOptions: {
           // https://purgecss.com/configuration.html#options
