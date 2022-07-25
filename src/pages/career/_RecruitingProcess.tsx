f8d003f36564199bd572c589b385739bef0480e0
codersbros <p.grden@codersbros.pl>
2022-07-25 14:23:38 +0200

    change image in RecruitingProcess.tsx

--- src/pages/career/_RecruitingProcess.tsx
+++ src/pages/career/_RecruitingProcess.tsx
@@ -79,7 +79,7 @@ const RecruitingProcess: React.FC<Props> = ({ recruting_image2_title, recruting_
         isOnlyOnMobile: false,
       },
       {
-        image: '/images/career/recruiting/congrats.png',
+        image: '/images/career/recruiting/recruitment_process_congrats_500x408.png',
         title: 'congrats! you are a part of a bright team!',
         isOnlyOnMobile: true,
       },
