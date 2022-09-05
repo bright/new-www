6c9e12c560f58cee3a51e7e72ffe8e6d30630533
CodersBros <p.grden@codersbros.pl>
2022-09-05 09:03:51 +0200

    fix buttonBlockConfig.tsx

--- src/cms/buttonBlockConfig.tsx
+++ src/cms/buttonBlockConfig.tsx
@@ -65,7 +65,7 @@ export const buttonBlockConfig = {
       '</div>' +
       `<a href="${obj.link}">` +
       '<button>' +
-      +obj.button +
+      obj.button +
       '</button>' +
       '</a>' +
       '</div>'
