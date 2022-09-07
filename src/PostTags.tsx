d8ec8dc072aab2baef07ead957a7ca79dfdd7e36
CodersBros <p.grden@codersbros.pl>
2022-09-07 10:03:37 +0200

    change font family tags in blog post

--- src/PostTags.tsx
+++ src/PostTags.tsx
@@ -6,25 +6,36 @@ const Tags = styled.p`
   align-items: center;
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
+  justify-content: flex-end;
   gap: ${variables.pxToRem(10)};
+  font-family: ${variables.font.customtext.lato};
+  color: ${variables.color.text2};
 
-  @media ${variables.device.tablet} {
-    justify-content: flex-end;
-    align-items: flex-end;
-  }
   @media ${variables.device.mobile} {
     justify-content: center;
   }
+`
+const Tag = styled.span`
+  align-items: center;
+  background-color: hsl(0deg, 0%, 96%);
+  border-radius: 4px;
+  color: ${variables.color.text2};
+  font-family: ${variables.font.customtext.lato};
+  display: inline-flex;
+  font-size: 0.75rem;
+  height: 2em;
+  justify-content: center;
+  line-height: 1.5;
+  padding-left: 0.75em;
+  padding-right: 0.75em;
+  white-space: nowrap;
 `
 
 export const PostTags = ({ tags }: { tags: string[] }) => {
   return (
     <Tags>
       {tags.map((tag, index) => (
-        <span className='tag' key={'tag-' + index}>
-          {tag}
-        </span>
+        <Tag key={'tag-' + index}>{tag}</Tag>
       ))}
     </Tags>
   )
