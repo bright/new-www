ae11f2422548b16a3e11fa653ecad97fb27827fb
CodersBros <p.grden@codersbros.pl>
2022-09-07 12:30:57 +0200

    change layout RelatedPosts.tsx

--- src/templates/post/RelatedPosts.tsx
+++ src/templates/post/RelatedPosts.tsx
@@ -1,6 +1,6 @@
-import { graphql, Link } from 'gatsby'
+import { Link } from 'gatsby'
 import React from 'react'
-import { CustomSectionTitle, MoreButton } from '../../components/shared'
+import { CustomSectionTitle } from '../../components/shared'
 import { routeLinks } from '../../config/routing'
 import { createBlogRelatedPosts } from '../../models/creator'
 import { allMarkdownRemarkData } from '../../models/gql'
@@ -10,8 +10,34 @@ import variables from '../../styles/variables'
 import { useEffect } from 'react'
 import { useState } from 'react'
 
-const RelatedMoreButton = styled(MoreButton)`
-  margin-top: ${variables.pxToRem(105)};
+const RelatedMoreButton = styled.button`
+  border: 1px solid black;
+  background: white;
+  font-family: 'Montserrat', sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  -webkit-letter-spacing: 0;
+  -moz-letter-spacing: 0;
+  -ms-letter-spacing: 0;
+  letter-spacing: 0;
+  color: #000000;
+  opacity: 1;
+  padding: 1rem 4rem;
+  font-size: 1.125rem;
+  cursor: pointer;
+  -webkit-transition: all 0.3s ease-out;
+  transition: all 0.3s ease-out;
+  margin: ${variables.pxToRem(105)} auto 0;
+  display: block;
+  &:hover {
+    color: rgb(255, 255, 255);
+    background: rgb(0, 0, 0);
+  }
+
+  @media ${variables.device.mobile} {
+    width: 100%;
+    margin: ${variables.pxToRem(64)} 0 0;
+  }
 `
 
 interface RelatedPostsProps {
