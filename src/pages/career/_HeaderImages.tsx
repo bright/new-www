0820f2beb40bed7c8a21c59b71df630bd0f0f689
Izabela <82568642+izabelapawlik@users.noreply.github.com>
2022-09-27 15:43:27 +0200

    Update _HeaderImages.tsx

--- src/pages/career/_HeaderImages.tsx
+++ src/pages/career/_HeaderImages.tsx
@@ -155,7 +155,7 @@ const HeaderImages: React.FC = () => {
         </ImagesVertical>
         <StaticImage
           objectFit={'cover'}
-          src='../../../static/images/career/header/damian_college_career.png'
+          src='../../../static/images/career/header/damian_homepage_large.png'
           alt={'Software developer career Poland'}
           quality={100}
           height={820}
