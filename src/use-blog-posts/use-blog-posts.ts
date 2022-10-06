3849747fbdb6542ed1de70bec4140f4420a18dd8
CodersBros <p.grden@codersbros.pl>
2022-10-06 23:00:29 +0200

    add sort by dateModified to use-blog-post.ts

--- src/use-blog-posts/use-blog-posts.ts
+++ src/use-blog-posts/use-blog-posts.ts
@@ -2,5 +2,12 @@ import { useTopPublishedBlogPostsQuery } from './use-top-published-blog-posts-qu
 import { toBlogPost } from './blog-post-frontmatter-query-result'
 
 export const useTopBlogPosts = () => {
-  return useTopPublishedBlogPostsQuery().map(toBlogPost)
+  return useTopPublishedBlogPostsQuery()
+    .map(toBlogPost)
+    .sort(function (a, b) {
+      return (
+        new Date(b.dateModified).getTime() - new Date(a.dateModified).getTime() ||
+        new Date(b.date).getTime() - new Date(a.date).getTime()
+      )
+    })
 }
