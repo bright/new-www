dadd71f5cb638c49ca26162d1701983408a2edeb
Piotr Mionskowski <piotr.mionskowski@brightinventions.pl>
2022-10-27 10:39:47 +0200

    Register custom styles in cms from main

--- src/cms/cms.ts
+++ src/cms/cms.ts
@@ -1,8 +1,8 @@
 import CMS from 'netlify-cms-app'
-const { MdxControl, MdxPreview } = require("netlify-cms-widget-mdx");
+
+const { MdxControl, MdxPreview } = require('netlify-cms-widget-mdx')
 // this only seems to register styles in preview pane
 // or nowhere at all
-// import '../styles/main.scss'
 import { BlogPostPreview } from './BlogPostPreview'
 import { buttonBlockConfig } from './buttonBlockConfig'
 import { hiddenImageConfig } from './hiddenImageConfig'
@@ -10,9 +10,17 @@ import { importantInfoConfig } from './importantInfoConfig'
 import { quoteConfig } from './quoteConfig'
 import { TagsControl, TagsPreview } from './tags'
 
+import mainStyles from '!css-loader!sass-loader!../styles/main.scss'
 import quoteStyles from '!css-loader!../cms/styles/quote.css'
 
-console.log('registering custom editor & preview components')
+CMS.registerPreviewStyle(mainStyles.toString(), { raw: true })
+CMS.registerPreviewStyle(quoteStyles.toString(), { raw: true })
+
+
+console.log('registering custom editor & preview components', {
+  mainStyles,
+  quoteStyles
+})
 
 CMS.registerWidget('tags', TagsControl, TagsPreview)
 CMS.registerWidget('mdx', MdxControl, MdxPreview)
@@ -22,5 +30,5 @@ CMS.registerEditorComponent({ ...buttonBlockConfig })
 CMS.registerEditorComponent({ ...hiddenImageConfig })
 CMS.registerEditorComponent({ ...importantInfoConfig })
 CMS.registerEditorComponent({ ...quoteConfig })
-CMS.registerPreviewStyle(quoteStyles.toString(), { raw: true })
+
 
