f57ad68f472284972e59bef8655c0a2886d89ca7
CodersBros <p.grden@codersbros.pl>
2022-11-03 10:15:29 +0100

    add gatsby-plugin-gdpr-cookies, react-cookie-consent, react-modal

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,23 +11,25 @@ const isDevelop = !process.env.GATSBY_ACTIVE_ENV
 
 const generateRobotsContent = !isDevelop
 
-
 export default {
   siteMetadata,
+
   plugins: [
     // Make sure this plugin is first in the array of plugins
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: isProduction ? ['UA-29336006-1', 'G-H4MTQGSVD3', 'AW-10942749476'] : []
-      }
+        trackingIds: isProduction ? ['UA-29336006-1', 'G-H4MTQGSVD3', 'AW-10942749476'] : [],
+      },
     },
-    !isDevelop ? {
-      resolve: `gatsby-plugin-facebook-pixel`,
-      options: {
-        pixelId: `${process.env.GATSBY_ACTIVE_ENV === 'production' ? '1256554391514599' : ''}`
-      }
-    } : null,
+    !isDevelop
+      ? {
+          resolve: `gatsby-plugin-facebook-pixel`,
+          options: {
+            pixelId: `${process.env.GATSBY_ACTIVE_ENV === 'production' ? '1256554391514599' : ''}`,
+          },
+        }
+      : null,
 
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
@@ -44,79 +46,103 @@ export default {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'static/images/favicon.png' // This path is relative to the root of the site.
-      }
+        icon: 'static/images/favicon.png', // This path is relative to the root of the site.
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: `${__dirname}/static/images`
-      }
+        path: `${__dirname}/static/images`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/projects`,
-        name: `md-projects`
-      }
+        name: `md-projects`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/faqs`,
-        name: `md-faqs`
-      }
+        name: `md-faqs`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/our-areas`,
-        name: `md-our_areas`
-      }
+        name: `md-our_areas`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/members`,
-        name: `md-members`
-      }
+        name: `md-members`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/jobs`,
-        name: `md-jobs`
-      }
+        name: `md-jobs`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/blog`,
-        name: `md-jobs`
-      }
+        path: `${__dirname}/content/blog/`,
+        name: `md-jobs`,
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'fonts',
-        path: `${__dirname}/static/fonts/`
-      }
+        path: `${__dirname}/static/fonts/`,
+      },
     },
     {
       resolve: `gatsby-plugin-disqus`,
       options: {
-        shortname: `brightinventions`
-      }
+        shortname: `brightinventions`,
+      },
     },
     {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
-          include: /assets/
-        }
-      }
+          include: /assets/,
+        },
+      },
     },
+    {
+      resolve: `gatsby-plugin-gdpr-cookies`,
+      options: {
+        googleTagManager: {
+          trackingId: isProduction ? ['UA-29336006-1', 'G-H4MTQGSVD3', 'AW-10942749476'] : '', // leave empty if you want to disable the tracker
+          cookieName: 'gatsby-gdpr-google-tagmanager', // default
+          dataLayerName: 'dataLayer', // default
+        },
+        facebookPixel: {
+          pixelId: `${process.env.GATSBY_ACTIVE_ENV === 'production' ? '1256554391514599' : ''}`, // leave empty if you want to disable the tracker
+          cookieName: 'gatsby-gdpr-facebook-pixel', // default
+        },
+
+        hotjar: {
+          hjid: isProduction ? '2864857' : '',
+          hjsv: isProduction ? '6' : '',
+          cookieName: 'gatsby-gdpr-hotjar', // default
+        },
+
+        // defines the environments where the tracking should be available  - default is ["production"]
+        environments: ['production', 'development'],
+      },
+    },
+
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-purgecss`,
@@ -129,7 +155,7 @@ export default {
           'prismjs/',
           'style/bulma/sass/components/modal.sass',
           '/swiper.min.css',
-          '/pagination.min.css'
+          '/pagination.min.css',
         ], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
         purgeCSSOptions: {
@@ -145,16 +171,16 @@ export default {
             'content',
             'modal is-active',
             'modal-close',
-            'modal-close is-large'
-          ] // Don't remove this selector
-        }
+            'modal-close is-large',
+          ], // Don't remove this selector
+        },
         // More options defined here https://purgecss.com/configuration.html#options
-      }
+      },
     },
 
     {
       resolve: `gatsby-plugin-mdx`,
-      options: gatsbyMdxOptions
+      options: gatsbyMdxOptions,
     },
     {
       resolve: `gatsby-plugin-netlify-cms`,
@@ -180,75 +206,81 @@ export default {
               fs: false,
               child_process: false,
               module: false,
-              assert: false
-            }
+              assert: false,
+            },
           }
           // required by netlify-cms-widget-mdx
           config.plugins = [
             ...config.plugins,
             new ProvidePlugin({
-              process: path.resolve(path.join(__dirname, 'node_modules', 'process/browser'))
+              process: path.resolve(path.join(__dirname, 'node_modules', 'process/browser')),
             }),
             // required by @mdx-js/mdx/node_modules/jsesc/jsesc.js
             new ProvidePlugin({
-              Buffer: path.resolve(path.join(__dirname, 'node_modules', 'buffer'))
-            })
+              Buffer: path.resolve(path.join(__dirname, 'node_modules', 'buffer')),
+            }),
           ]
-        }
-      }
+        },
+      },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
-    generateRobotsContent ? {
-      resolve: `gatsby-plugin-sitemap`,
-      options: {
-        output: '/'
-      }
-    } : null,
-    generateRobotsContent ? {
-      resolve: `gatsby-plugin-feed`,
-      options: gatsbyPluginFeedOptions(siteMetadata)
-    } : null,
+    generateRobotsContent
+      ? {
+          resolve: `gatsby-plugin-sitemap`,
+          options: {
+            output: '/',
+          },
+        }
+      : null,
+    generateRobotsContent
+      ? {
+          resolve: `gatsby-plugin-feed`,
+          options: gatsbyPluginFeedOptions(siteMetadata),
+        }
+      : null,
     {
       resolve: 'gatsby-plugin-s3',
       options: {
         bucketName: process.env.DEPLOY_S3_BUCKET_NAME || 'brightinventions-pl-website-content',
         protocol: 'https',
         hostname: siteUrl.hostname,
-        region: 'eu-central-1'
-      }
+        region: 'eu-central-1',
+      },
     },
     {
       resolve: `gatsby-plugin-loadable-components-ssr`,
       options: {
         // Whether replaceHydrateFunction should call ReactDOM.hydrate or ReactDOM.render
         // Defaults to ReactDOM.render on develop and ReactDOM.hydrate on build
-        useHydrate: true
-      }
+        useHydrate: true,
+      },
     },
-    generateRobotsContent ? {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        host: siteUrl.href,
-        sitemap: `${siteUrl.href}sitemap-index.xml`,
-        env: {
-          staging: {
-            policy: [{ userAgent: '*', disallow: ['/'] }]
+    generateRobotsContent
+      ? {
+          resolve: 'gatsby-plugin-robots-txt',
+          options: {
+            host: siteUrl.href,
+            sitemap: `${siteUrl.href}sitemap-index.xml`,
+            env: {
+              staging: {
+                policy: [{ userAgent: '*', disallow: ['/'] }],
+              },
+              production: {
+                policy: [{ userAgent: '*', allow: '/' }],
+              },
+            },
           },
-          production: {
-            policy: [{ userAgent: '*', allow: '/' }]
-          }
         }
-      }
-    } : null,
+      : null,
     {
       resolve: `gatsby-plugin-hotjar`,
       options: {
         includeInDevelopment: false, // optional parameter to include script in development
         id: isProduction ? '2864857' : '',
-        sv: isProduction ? '6' : ''
-      }
+        sv: isProduction ? '6' : '',
+      },
     },
 
     {
@@ -269,15 +301,17 @@ export default {
               }
             `,
             transformer: ({
-                            data: {
-                              allMdx: { posts }
-                            }
-                          }: { data: { allMdx: { posts: Node[] } } }) => ({
-              tags: [...new Set(posts.flatMap(post => post.frontmatter?.tags ?? []))]
-            })
-          }
-        ]
-      }
+              data: {
+                allMdx: { posts },
+              },
+            }: {
+              data: { allMdx: { posts: Node[] } }
+            }) => ({
+              tags: [...new Set(posts.flatMap(post => post.frontmatter?.tags ?? []))],
+            }),
+          },
+        ],
+      },
     },
     {
       resolve: 'gatsby-plugin-page-progress',
@@ -287,12 +321,12 @@ export default {
         prependToBody: false,
         color: `#f7931e`,
         footerHeight: 1000,
-        headerHeight: 73
-      }
-    }
+        headerHeight: 73,
+      },
+    },
   ].filter(isDefined),
   mapping: {
     'Mdx.frontmatter.faqs': `Mdx.frontmatter.faqs_id`,
-    'Mdx.frontmatter.project': `Mdx.frontmatter.project_id`
-  }
+    'Mdx.frontmatter.project': `Mdx.frontmatter.project_id`,
+  },
 }
