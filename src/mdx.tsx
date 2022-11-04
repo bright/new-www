0cf311af46c5d1f9ebe4a9f9ac76b5fe267cab01
Piotr Mionskowski <piotr.mionskowski@brightinventions.pl>
2022-11-04 11:37:47 +0100

    Use updated Youtube embed

--- src/mdx.tsx
+++ src/mdx.tsx
@@ -1,27 +1,6 @@
-import YouTube, { YouTubeProps } from 'react-youtube'
 import { MDXProvider } from '@mdx-js/react'
 import React, { ReactNode } from 'react'
-import styled from 'styled-components'
-
-const YouTubeStyled = styled(YouTube)`
-  // hardcoded based on https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-remark-responsive-iframe/src/index.js#L59
-  padding-bottom: 56.49999999999999%;
-  position: relative;
-  height: 0;
-  overflow: hidden;
-`
-
-const YouTubeWrapper = (props: YouTubeProps) => {
-  props = {
-    opts: {
-      width: '100%',
-      height: '100%'
-    },
-    ...props
-  }
-  return <YouTubeStyled  {...props} />
-}
-
+import { YouTubeWrapper } from './youtube'
 
 export const mdxComponents = {
   YouTube: YouTubeWrapper
