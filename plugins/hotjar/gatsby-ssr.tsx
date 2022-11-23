9437675f4d58eee6c0fc492f8a551a455b88b2d7
Piotr Mionskowski <piotr.mionskowski@brightinventions.pl>
2022-11-23 15:17:12 +0100

    Fix hotjar loading

--- plugins/hotjar/gatsby-ssr.tsx
+++ plugins/hotjar/gatsby-ssr.tsx
@@ -7,7 +7,7 @@ export const onRenderBody: GatsbySSR['onRenderBody'] = ({ setHeadComponents }, o
     const hotJarInitScript = `
 function loadHotjarScript(h,o,t,j,a,r){
     h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-    h._hjSettings={hjid:' + id + ',hjsv:' + sv + '};
+    h._hjSettings={ hjid: ${id}, hjsv: ${sv} };
     a=o.getElementsByTagName('head')[0];
     r=o.createElement('script');r.async=1;
     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
