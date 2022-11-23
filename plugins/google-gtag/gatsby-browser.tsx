a84034e13f609d7e3a35e42d61aa09cf24563aab
Piotr Mionskowski <piotr.mionskowski@brightinventions.pl>
2022-11-23 19:06:53 +0100

    Report article tags and publish time as additional dimensions in google analytics

--- plugins/google-gtag/gatsby-browser.tsx
+++ plugins/google-gtag/gatsby-browser.tsx
@@ -1,12 +1,39 @@
 import { GatsbyBrowser } from 'gatsby'
+import { gtagOrFallback } from './gtag-or-fallback'
+
+// TODO: this would be the same in pixel or hotjar
+// how do we unify that?
+const supportedMetaTag = {
+  'article:published_time': 'article_published_at',
+  'article:tag': 'tags',
+}
+
+function collectMetaTags() {
+  return Array.from(document.querySelectorAll('meta')).map(meta => ({
+    key: meta.getAttribute('property') ?? meta.name,
+    value: meta.content,
+  }))
+}
+
+function pageViewDimensionsFromMeta() {
+  return collectMetaTags()
+    .filter(({ key }) => key in supportedMetaTag)
+    .reduce((acc, { key, value }) => {
+      const metaKeyNameAsDimensionName = supportedMetaTag[key as keyof typeof supportedMetaTag]
+      return Object.assign(acc, {
+        [metaKeyNameAsDimensionName]: value,
+      })
+    })
+}
 
 export const onRouteUpdate: GatsbyBrowser['onRouteUpdate'] = args => {
   const location = args.location
   // directly from gatsby-plugin-google-gtag
   const sendPageView = function sendPageView() {
     const pagePath = location ? location.pathname + location.search + location.hash : undefined
-    window.gtag('event', 'page_view', {
+    gtagOrFallback()('event', 'page_view', {
       page_path: pagePath,
+      ...pageViewDimensionsFromMeta(),
     })
   }
 
