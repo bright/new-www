759ebf10638d61c7b3da6acdf8cf1d8753c850f2
CodersBros <p.grden@codersbros.pl>
2022-12-02 13:22:18 +0100

    remove unused import

--- src/templates/ProjectTemplate.tsx
+++ src/templates/ProjectTemplate.tsx
@@ -83,15 +83,7 @@ const Template: React.FC<{ data: { mdx: any } }> = ({ data, children }) => {
 
   return (
     <Page>
-      <HelmetMetaData
-        title={title}
-        description={description}
-        url={pathname}
-        type='product'
-        image={image}
-        alt={alt}
-        twitterType='summary_large_image'
-      />
+      <HelmetMetaData title={title} description={description} type='product' image={image} alt={alt} />
       <Container className='container' id='project'>
         <article className='section'>
           <Title>{title}</Title>
