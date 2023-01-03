a32fa654e14140e48f7a3b139984868b2f69ec2e
CodersBros <p.grden@codersbros.pl>
2023-01-03 16:47:43 +0100

    change layout OurServiceTemplate.tsx

--- src/templates/styled/OurServiceTemplateStyled.ts
+++ src/templates/styled/OurServiceTemplateStyled.ts
@@ -302,7 +302,11 @@ export const OurServicePageTitle = styled(CustomPageTitle)`
     line-height: ${variables.pxToRem(90)};
   }
   @media ${variables.device.mobile} {
+<<<<<<< HEAD
     font-size: ${clampBuilder(200, 581, 40, 52)};
+=======
+    font-size: ${variables.pxToRem(52)};
+>>>>>>> efbdf024 (change layout OurServiceTemplate.tsx)
     line-height: ${variables.pxToRem(63)};
   }
 `
