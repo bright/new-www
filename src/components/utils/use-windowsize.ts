043b1e588934a65134b5bf2b9e3ae3cd83559484
CodersBros <p.grden@codersbros.pl>
2023-01-05 11:15:45 +0100

    fixed render image using useWindowSize hook

--- src/components/utils/use-windowsize.ts
+++ src/components/utils/use-windowsize.ts
@@ -1,31 +1,28 @@
-import React from "react";
+import React from 'react'
 import { useEffect, useState } from 'react'
 
-
 interface WindowSize {
-    width: number
-   
-  }
+  width: number | undefined
+}
 
-
-export const useWindowSize = ():WindowSize => {
-  
-   
-  const isSSR = typeof window !== "undefined";
+export const useWindowSize = (): WindowSize => {
+  const isSSR = typeof window !== 'undefined'
   const [windowSize, setWindowSize] = useState<WindowSize>({
-    width: isSSR ? window.innerWidth : 500 });
+    width: isSSR ? window.innerWidth : undefined,
+  })
+  console.log(windowSize)
 
   function changeWindowSize() {
-    setWindowSize({ width: window.innerWidth });
+    setWindowSize({ width: window.innerWidth })
   }
 
   useEffect(() => {
-    window.addEventListener("resize", changeWindowSize);
+    window.addEventListener('resize', changeWindowSize)
 
     return () => {
-      window.removeEventListener("resize", changeWindowSize);
-    };
-  }, []);
+      window.removeEventListener('resize', changeWindowSize)
+    }
+  }, [])
 
-  return windowSize;
+  return windowSize
 }
