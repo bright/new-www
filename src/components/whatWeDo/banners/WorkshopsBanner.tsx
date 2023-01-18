a40d5184297459418463e283adcb7e318f355e9f
CodersBros <p.grden@codersbros.pl>
2023-01-18 17:15:25 +0100

    unification  mobile device buttons

--- src/components/whatWeDo/banners/WorkshopsBanner.tsx
+++ src/components/whatWeDo/banners/WorkshopsBanner.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { BannerLink, Button, Container, ContentWrapper, Text } from './styles'
+import { MoreButton } from '../../shared'
+import { Container, ContentWrapper, Text, OurAreasButtonWrapper } from './styles'
 
 const WorkshopsBanner = () => {
   return (
     <Container>
       <ContentWrapper>
         <Text>we organise Agile workshops for teams</Text>
-
-        <BannerLink to='/our-areas/agile-workshops'>
-          <Button>book free consultation</Button>
-        </BannerLink>
+        <OurAreasButtonWrapper>
+          <MoreButton href='/our-areas/agile-workshops' className='our-areas-button'>
+            book free consultation
+          </MoreButton>
+        </OurAreasButtonWrapper>
       </ContentWrapper>
     </Container>
   )
