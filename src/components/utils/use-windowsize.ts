b0dadad624fe65b8ba7e552796417dd6ef8895d0
CodersBros <p.grden@codersbros.pl>
2023-01-30 10:34:45 +0100

    add height to WindowsSize custom hook

--- src/components/utils/use-windowsize.ts
+++ src/components/utils/use-windowsize.ts
@@ -3,16 +3,18 @@ import { useEffect, useState } from 'react'
 
 interface WindowSize {
   width: number
+  height?: number
 }
 
 export const useWindowSize = (): WindowSize => {
   const isSSR = typeof window !== 'undefined'
   const [windowSize, setWindowSize] = useState<WindowSize>({
     width: isSSR ? window.innerWidth : 500,
+    height: isSSR ? window.innerHeight : 500,
   })
 
   function changeWindowSize() {
-    setWindowSize({ width: window.innerWidth })
+    setWindowSize({ width: window.innerWidth, height: window.innerHeight })
   }
 
   useEffect(() => {
