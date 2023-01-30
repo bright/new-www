c014912c4338e4dcba2d4f7369728dcb44d9a6f1
CodersBros <p.grden@codersbros.pl>
2023-01-30 10:36:47 +0100

    change layout OurServiceTemplate.tsx

--- src/templates/styled/OurServiceTemplateStyled.ts
+++ src/templates/styled/OurServiceTemplateStyled.ts
@@ -390,7 +390,5 @@ export const OurServiceFlexWraper = styled(FlexWrapper)`
 export const MobileOurServiceFlexWrapper = styled(FlexWrapper)`
   @media ${variables.device.mobile} {
     height: calc(100vh - 90px);
-
-    height: calc(100dvh - 90px);
   }
 `
