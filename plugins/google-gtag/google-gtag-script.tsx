51e5cdd1bfb7d06b9b22831d4dc4258ccad70cbe
Piotr Mionskowski <piotr.mionskowski@brightinventions.pl>
2023-03-15 14:10:13 +0100

    Restore old way of initing gtag

--- plugins/google-gtag/google-gtag-script.tsx
+++ plugins/google-gtag/google-gtag-script.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { ComponentProps, useEffect } from 'react'
 import { PluginOptions } from 'gatsby'
 import { Script } from 'gatsby-script'
 import { googleTagManagerUrl } from './google-tag-manager-url'
@@ -6,27 +6,8 @@ import { consentToGtagValue } from './consent-to-gtag-value'
 import { WindowLocation } from '@reach/router'
 import { isConnectedToGoogleGtagAssistant, setIsConnectedToGoogleGtagAssistant } from './google-gtag-assistant'
 
-export const GoogleGtagScript = ({ options, location }: { options: PluginOptions; location?: WindowLocation }) => {
-  const trackingIds = options.trackingIds ?? []
-
-  if (Array.isArray(trackingIds) && trackingIds.length > 0) {
-    const isConnectedToGtagDebugger = isConnectedToGoogleGtagAssistant(location)
-    const partytownEnabled = options.partytownEnabled && !isConnectedToGtagDebugger
-    const scriptLoadStrategy = partytownEnabled ? 'off-main-thread' : 'post-hydrate'
-
-    useEffect(() => {
-      setIsConnectedToGoogleGtagAssistant(isConnectedToGtagDebugger)
-    }, [])
-
-    const firstTrackingTag = trackingIds[0]
-    // https://developers.google.com/tag-platform/gtagjs/install
-    const partytownForwards = partytownEnabled ? ['gtag', 'dataLayer.push'] : []
-    console.debug('gtag', { scriptLoadStrategy, partytownForwards })
-    return (
-      <>
-        <Script src={googleTagManagerUrl(firstTrackingTag)} strategy={scriptLoadStrategy} forward={partytownForwards} />
-        <Script id='gtag-config' strategy={scriptLoadStrategy}>
-          {`
+export function gtagInitScript(trackingIds: any[]) {
+  return `
 window.dataLayer = window.dataLayer || [];
 function gtag(){
   console.debug('gtag', arguments);  
@@ -46,7 +27,35 @@ ${trackingIds
     return `gtag('config', '${trackingId}');`
   })
   .join('\n')}
-`}
+`
+}
+
+export const GoogleGtagScript = ({ options, location }: { options: PluginOptions; location?: WindowLocation }) => {
+  const trackingIds = options.trackingIds ?? []
+
+  if (Array.isArray(trackingIds) && trackingIds.length > 0) {
+    const isConnectedToGtagDebugger = isConnectedToGoogleGtagAssistant(location)
+    const partytownEnabled = options.partytownEnabled as boolean && !isConnectedToGtagDebugger
+
+    if(!partytownEnabled){
+      return <></>;
+    }
+
+    const scriptLoadStrategy = partytownEnabled ? 'off-main-thread' : 'post-hydrate'
+
+    useEffect(() => {
+      setIsConnectedToGoogleGtagAssistant(isConnectedToGtagDebugger)
+    }, [])
+
+    const firstTrackingTag = trackingIds[0]
+    // https://developers.google.com/tag-platform/gtagjs/install
+    const partytownForwards = partytownEnabled ? ['gtag', 'dataLayer.push'] : []
+    console.debug('gtag', { scriptLoadStrategy, partytownForwards })
+    return (
+      <>
+        <Script src={googleTagManagerUrl(firstTrackingTag)} strategy={scriptLoadStrategy} forward={partytownForwards} />
+        <Script id='gtag-config' strategy={scriptLoadStrategy}>
+          {gtagInitScript(trackingIds)}
         </Script>
       </>
     )
