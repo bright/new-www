ff50ec1691ab6232ecf15c7a8f80779c008ccf39
CodersBros <p.grden@codersbros.pl>
2023-03-16 12:16:19 +0100

    register ebook cms component

--- src/mdx.tsx
+++ src/mdx.tsx
@@ -4,13 +4,16 @@ import { BrightYouTubeEmbed } from './youtube-embed'
 import { BrightInstagramEmbed } from './instagram-embed'
 import { GiphyEmbed } from './giphy-embed'
 import { BrightTwitterEmbed } from './twitter-embed'
+import { EbookDynamic } from './ebook-dynamic'
 
 export const mdxComponents = {
   YouTubeEmbed: BrightYouTubeEmbed,
   InstagramEmbed: BrightInstagramEmbed,
   TwitterEmbed: BrightTwitterEmbed,
-  GiphyEmbed: GiphyEmbed
+  GiphyEmbed: GiphyEmbed,
+  EbookDynamic: EbookDynamic,
 }
 
-export const MDXComponentsWrapper = ({ children }: PropsWithChildren<any>) =>
+export const MDXComponentsWrapper = ({ children }: PropsWithChildren<any>) => (
   <MDXProvider components={mdxComponents}>{children}</MDXProvider>
+)
