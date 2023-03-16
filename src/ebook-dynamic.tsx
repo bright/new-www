1b78ff6f15601351717c349f0c37d0d7ea52be1c
CodersBros <p.grden@codersbros.pl>
2023-03-16 12:13:49 +0100

    create dynamic ebook components

---
+++ src/ebook-dynamic.tsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import { CustomSectionTitle, FlexWrapper, MoreButton } from './components/shared'
+import styled from 'styled-components'
+import variables from './styles/variables'
+import { useEbookForm } from './components/utils/ebook-form/use-ebook-form'
+import { EbookDynamicForm } from './ebook-dynamic-form'
+import { EbookThankYouPage } from './ebook-dynamic-thank-you-page'
+import { ErrorMessage } from './components/shared/contact/styles'
+
+const EbookSection = styled.section`
+  margin: ${variables.pxToRem(48)} 0;
+  padding: ${variables.pxToRem(32)} ${variables.pxToRem(56)};
+  background: ${variables.color.lighterGrey};
+  & form {
+    padding: ${variables.pxToRem(16)} 0 0;
+  }
+  @media ${variables.device.tablet} {
+    padding: ${variables.pxToRem(32)} ${variables.pxToRem(36)};
+  }
+`
+
+const MoreButtonWrapper = styled.div`
+  & button {
+    @media ${variables.device.tabletXL} {
+      margin-top: ${variables.pxToRem(16)};
+    }
+    @media ${variables.device.tablet} {
+      margin-top: ${variables.pxToRem(48)};
+    }
+  }
+`
+
+const EbookSectionTitle = styled(CustomSectionTitle)`
+  font-size: ${variables.pxToRem(40)};
+  line-height: ${variables.pxToRem(48.76)};
+  font-weight: 800;
+`
+
+export interface EbookDynamicProps {
+  section_title: string
+  ebook_name: string
+  imageSrc: string
+  imageAlt: string
+  description: string
+}
+
+export const EbookDynamic: React.FC<EbookDynamicProps> = ({
+  section_title,
+  ebook_name,
+  imageSrc,
+  imageAlt,
+  description,
+}) => {
+  const { value, setPolicy, handleSubmit, setEmail, setName } = useEbookForm(ebook_name)
+  const formRef = React.useRef<HTMLFormElement>(null)
+
+  return (
+    <EbookSection>
+      <EbookSectionTitle
+        margin='0 0 32px'
+        laptopMargin='0 0 32px'
+        tabletXLMargin='0 0 32px'
+        tabletMargin='0 0 32px'
+        mobileMargin='0 0 32px'
+      >
+        {section_title}
+      </EbookSectionTitle>
+      <FlexWrapper tabletDirection='column' tabletGap='32px' desktopItems='center'>
+        <img alt={imageAlt} src={imageSrc} />
+        {!value.ebookResponse.ebook.url ? (
+          <EbookDynamicForm
+            description={description}
+            setEmail={setEmail}
+            setName={setName}
+            setPolicy={setPolicy}
+            handleSubmit={handleSubmit}
+            value={value}
+            formRef={formRef}
+          />
+        ) : (
+          <EbookThankYouPage />
+        )}
+      </FlexWrapper>
+      <MoreButtonWrapper>
+        <ErrorMessage> {value.errorMsg && value.errorMsg}</ErrorMessage>
+        {!value.ebookResponse.ebook.url ? (
+          <MoreButton
+            isSubmit
+            isPrimary
+            onClick={() => {
+              formRef.current?.dispatchEvent(new Event('submit', { cancelable: true, bubbles: true }))
+            }}
+          >
+            get free ebook
+          </MoreButton>
+        ) : (
+          <a href={value.ebookResponse.ebook.url} target='_blank'>
+            click to open the ebook
+          </a>
+        )}
+      </MoreButtonWrapper>
+    </EbookSection>
+  )
+}
