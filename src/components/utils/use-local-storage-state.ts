6d0251eba38c40f54f83f3b8f5694e562290557d
CodersBros <p.grden@codersbros.pl>
2023-03-16 14:31:30 +0100

    add ssr check in use local storage state

--- src/components/utils/use-local-storage-state.ts
+++ src/components/utils/use-local-storage-state.ts
@@ -1,32 +1,34 @@
 
 import React from 'react'
-
+import { isBrowser } from '../../utils'
 
 function useLocalStorageState(
- key: string,
- defaultValue = '',
- {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+  key: string,
+  defaultValue = '',
+  { serialize = JSON.stringify, deserialize = JSON.parse } = {}
 ) {
- const [state, setState] = React.useState(() => {
-   const valueInLocalStorage = window.localStorage.getItem(key)
-   if (valueInLocalStorage) {
-     return deserialize(valueInLocalStorage)
-   }
-   return defaultValue
- })
+  const [state, setState] = React.useState(() => {
+    const valueInLocalStorage = isBrowser() ? window.localStorage.getItem(key) : ''
+    if (valueInLocalStorage) {
+      return deserialize(valueInLocalStorage)
+    }
+    return defaultValue
+  })
 
- const prevKeyRef = React.useRef(key)
+  const prevKeyRef = React.useRef(key)
 
- React.useEffect(() => {
-   const prevKey = prevKeyRef.current
-   if (prevKey !== key) {
-     window.localStorage.removeItem(prevKey)
-   }
-   prevKeyRef.current = key
-   window.localStorage.setItem(key, serialize(state))
- }, [key, state, serialize])
+  React.useEffect(() => {
+    const prevKey = prevKeyRef.current
+    if (isBrowser()) {
+      if (prevKey !== key) {
+        window.localStorage.removeItem(prevKey)
+      }
+      prevKeyRef.current = key
+      window.localStorage.setItem(key, serialize(state))
+    }
+  }, [key, state, serialize])
 
- return [state, setState]
+  return [state, setState]
 }
 
 export {useLocalStorageState}
