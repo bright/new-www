f0cdfa7b578bcf75c2563e89807a1f0e2478d526
CodersBros <p.grden@codersbros.pl>
2023-03-16 12:17:32 +0100

    add use local storage hook to dynamic get and set items from local storage

---
+++ src/components/utils/use-local-storage-state.ts
@@ -0,0 +1,32 @@
+
+import React from 'react'
+
+
+function useLocalStorageState(
+ key: string,
+ defaultValue = '',
+ {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) {
+ const [state, setState] = React.useState(() => {
+   const valueInLocalStorage = window.localStorage.getItem(key)
+   if (valueInLocalStorage) {
+     return deserialize(valueInLocalStorage)
+   }
+   return defaultValue
+ })
+
+ const prevKeyRef = React.useRef(key)
+
+ React.useEffect(() => {
+   const prevKey = prevKeyRef.current
+   if (prevKey !== key) {
+     window.localStorage.removeItem(prevKey)
+   }
+   prevKeyRef.current = key
+   window.localStorage.setItem(key, serialize(state))
+ }, [key, state, serialize])
+
+ return [state, setState]
+}
+
+export {useLocalStorageState}
