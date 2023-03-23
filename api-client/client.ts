ac58ea97474ec6aa1d9b595b5a83ce7ae7bb60c1
CodersBros <p.grden@codersbros.pl>
2023-03-23 12:47:21 +0100

    call by origin location when isBrowser to render in ssr

--- api-client/client.ts
+++ api-client/client.ts
@@ -1,12 +1,15 @@
 import { DefaultApi } from './apis'
 import { isProduction } from '../src/helpers/deployEnv'
 import { Configuration, DefaultConfig } from './runtime'
-const currentLocationUrl = window.location.origin
+import { isBrowser } from './../src/utils'
+import { siteUrl } from './../src/site-metadata'
+
+const callUrl = isBrowser() ? window.location.origin : siteUrl
 
 const apiConfig = isProduction
   ? DefaultConfig
   : new Configuration({
-      basePath: new URL('/api', currentLocationUrl).toString(),
+      basePath: new URL('/api', callUrl).toString(),
     })
 
 export const apiClient = new DefaultApi(apiConfig)
