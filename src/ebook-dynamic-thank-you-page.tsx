62e126523927449355aed7a07932f20d15adb14b
CodersBros <p.grden@codersbros.pl>
2023-03-24 12:05:44 +0100

    change layout EbookThankYouPage.tsx

--- src/ebook-dynamic-thank-you-page.tsx
+++ src/ebook-dynamic-thank-you-page.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import variables from "./styles/variables";
 
-const SectionTitle = styled.p`
+const SectionThankYouPage = styled.div`
+	&& p:first-of-type {
 	font-size: ${variables.pxToRem(28)};
 	line-height: ${variables.pxToRem(34)};
 	font-weight: 900;
 	padding-bottom: ${variables.pxToRem(48)};
 	text-align: center;
-`;
-
+	}
+	`
 const SectionDescription = styled.p`
 	font-family: ${variables.font.lato};
 	font-size: ${variables.pxToRem(20)};
@@ -19,12 +20,12 @@ const SectionDescription = styled.p`
 
 export const EbookThankYouPage = () => {
 	return (
-		<div>
-			<SectionTitle>enjoy reading</SectionTitle>
+		<SectionThankYouPage>
+			<p>enjoy reading</p>
 			<SectionDescription>
 				You’ll find the ebook in your inbox. Or simply click the link below to
 				download it.
 			</SectionDescription>
-		</div>
+		</SectionThankYouPage>
 	);
 };
