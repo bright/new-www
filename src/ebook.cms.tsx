97c453450a379a7e3ebb75f47ccf19c431ea31a8
CodersBros <p.grden@codersbros.pl>
2023-03-28 15:04:36 +0200

    change snake case to camel case to proper display props

--- src/ebook.cms.tsx
+++ src/ebook.cms.tsx
@@ -4,68 +4,68 @@ export const EbookCmsEditorComponent = {
   id: 'EbookDynamic',
   fields: [
     {
-      name: 'section_title',
+      name: 'sectionTitle',
       label: 'Section Title',
       widget: 'string',
     },
     {
-      name: 'ebook_name',
+      name: 'ebookName',
       label: 'Ebook name',
       widget: 'select',
       options: ebookNames,
     },
     {
-      name: 'ebook_description',
+      name: 'ebookDescription',
       label: 'Ebook Description',
       widget: 'string',
     },
     {
-      name: 'ebook_url',
+      name: 'ebookUrl',
       label: 'Ebook Url',
       widget: 'string',
     },
     {
-      name: 'ebook_alt',
+      name: 'ebookAlt',
       label: 'Ebook Image Alt',
       widget: 'string',
     },
     {
-      name: 'ebook_image',
+      name: 'ebookImage',
       label: 'Ebook Image',
       widget: 'image',
     },
   ],
   label: 'Ebook',
 
-  pattern: /^\s*<EbookDynamic\s+section_title=['"](.*?)['"]\s+ebook_name=['"](.*?)['"]\s+ebook_description=['"](.*?)['"]\s+ebook_image=['"](.*?)['"]\s+ebook_alt=['"](.*?)['"]\s+ebook_url=['"](.*?)['"]\s*\/>\s*$/,
+  pattern: /^\s*<EbookDynamic\s+sectionTitle=['"](.*?)['"]\s+ebookName=['"](.*?)['"]\s+ebookDescription=['"](.*?)['"]\s+ebookImage=['"](.*?)['"]\s+ebookAlt=['"](.*?)['"]\s+ebookUrl=['"](.*?)['"]\s*\/>\s*$/,
 
   fromBlock(match: RegExpMatchArray) {
     return {
-      section_title: match[1],
-      ebook_name: match[2],
-      ebook_description: match[3],
-      ebook_url: match[4],
-      ebook_image: match[5],
-      ebook_alt: match[6],
+      sectionTitle: match[1],
+      ebookName: match[2],
+      ebookDescription: match[3],
+      ebookUrl: match[4],
+      ebookImage: match[5],
+      ebookAlt: match[6],
     }
   },
   toBlock(props: {
-    section_title: string
-    ebook_name: string
-    ebook_description: string
-    ebook_url: string
-    ebook_image: string
-    ebook_alt: string
+    sectionTitle: string
+    ebookName: string
+    ebookDescription: string
+    ebookUrl: string
+    ebookImage: string
+    ebookAlt: string
   }) {
-    return `<EbookDynamic section_title='${props.section_title}' ebook_name='${props.ebook_name}' ebook_description='${props.ebook_description}'  ebook_url='${props.ebook_url}'  ebook_image='${props.ebook_image}' ebook_alt='${props.ebook_alt}' />`
+    return `<EbookDynamic sectionTitle='${props.sectionTitle}' ebookName='${props.ebookName}' ebookDescription='${props.ebookDescription}'  ebookUrl='${props.ebookUrl}'  ebookImage='${props.ebookImage}' ebookAlt='${props.ebookAlt}' />`
   },
   toPreview(props: {
-    section_title: string
-    ebook_name: string
-    ebook_description: string
-    ebook_url: string
-    ebook_image: string
-    ebook_alt: string
+    sectionTitle: string
+    ebookName: string
+    ebookDescription: string
+    ebookUrl: string
+    ebookImage: string
+    ebookAlt: string
   }) {
     return this.toBlock(props)
   },
