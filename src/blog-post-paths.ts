5647a2bb249fc6ac1f9d1d2bfd1132cc7c7de140
Piotr Mionskowski <piotr.mionskowski@brightinventions.pl>
2023-04-04 14:23:34 +0200

    Use `/` ending paths in rss

--- src/blog-post-paths.ts
+++ src/blog-post-paths.ts
@@ -3,9 +3,14 @@ import { urlSegmentForContentPath } from './url-segment-for-content-path'
 import { kebabCase } from './helpers/pathHelpers'
 
 export function blogListForTagGroupsBasePath(...tagGroups: TagGroup[]) {
-  return `/blog/${tagGroups.map(group => kebabCase(group.name.toLowerCase())).join('/')}`
+  if(tagGroups.length) {
+    const tags = tagGroups.map(group => kebabCase(group.name.toLowerCase())).join('/')
+    return `/blog/${tags}`
+  } else {
+    return `/blog/`
+  }
 }
 
 export function blogPostUrlPath(postNode: { frontmatter: { slug?: string }, internal: { contentFilePath: string } }) {
-  return '/blog/' + (postNode.frontmatter.slug || urlSegmentForContentPath(postNode.internal.contentFilePath))
+  return '/blog/' + (postNode.frontmatter.slug || urlSegmentForContentPath(postNode.internal.contentFilePath)) + '/'
 }
