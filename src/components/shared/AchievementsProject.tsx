45d10f73a64450f48535ca32263185af57ce3e3e
CodersBros <p.grden@codersbros.pl>
2023-06-01 10:33:49 +0200

    add AchievementsProject component for displaing achievements

---
+++ src/components/shared/AchievementsProject.tsx
@@ -0,0 +1,127 @@
+import React from 'react'
+import styled from 'styled-components'
+import { SectionBlack } from './index.styled'
+import variables from '../../styles/variables'
+import { FlexWrapper } from '.'
+
+
+const AchievementsSectionBlack = styled(SectionBlack)`
+  padding: ${variables.pxToRem(110)} ${variables.pxToRem(240)};
+  @media ${variables.device.laptop} {
+    padding: 6rem 11.125rem;
+  }
+  @media ${variables.device.tabletXL} {
+    padding: 6rem 9rem;
+  }
+  @media ${variables.device.tablet} {
+    padding: 6rem 11.125rem;
+  }
+  @media ${variables.device.mobile} {
+    padding: 6rem 0rem;
+  }
+`
+const AchievementsWrapper = styled.div`
+  max-width: 1440px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 0 auto;
+  & > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  @media ${variables.device.desktop} {
+    max-width: 1440px;
+  }
+  @media ${variables.device.laptop} {
+    max-width: 1080px;
+  }
+  @media ${variables.device.tabletXL} {
+    max-width: 992px;
+  }
+  @media ${variables.device.tablet} {
+    max-width: 600px;
+    flex-wrap: wrap;
+    & > div {
+      padding-bottom: 7.32rem;
+      flex: 1 1 50%;
+      &:nth-of-type(even) {
+        & > div {
+          align-self: center;
+        }
+      }
+      & > div {
+        align-self: center;
+      }
+
+      &:nth-of-type(n + 3) {
+        padding-bottom: 0;
+      }
+    }
+  }
+
+  @media ${variables.device.mobile} {
+    max-width: 360px;
+    flex-wrap: wrap;
+    & > div {
+      padding-bottom: 7.32rem;
+      flex: 1 1 50%;
+      &:nth-of-type(n + 3) {
+        padding-bottom: 0;
+      }
+    }
+  }
+`
+const Number = styled.div`
+    line-height: ${variables.pxToRem(88)};
+    font-size: ${variables.pxToRem(72)};
+    font-weight: 800;
+    @media ${variables.device.tabletXL} {
+      font-size: ${variables.pxToRem(54)};
+      line-height: ${variables.pxToRem(66)};
+    }
+    @media ${variables.device.tabletXL} {
+      font-size: ${variables.pxToRem(45)};
+      line-height: ${variables.pxToRem(55)};
+    }
+
+`
+const Label = styled.div`
+    line-height: ${variables.pxToRem(27)};
+    font-size: ${variables.pxToRem(22)};
+    opacity: .76;
+    @media ${variables.device.tabletXL} {
+      font-size: ${variables.pxToRem(16)};
+      line-height: ${variables.pxToRem(19)};
+    }
+`
+interface Achievement {
+  label: string,
+  number: string
+}
+interface AchievementsProps {
+  achievements?: Achievement[]
+}
+
+const AchievementsProject = ({ achievements }: AchievementsProps) => {
+
+
+  return (
+    <AchievementsSectionBlack >
+      <AchievementsWrapper>
+        {achievements && achievements.map(({ number, label }) => {
+          return (
+            <FlexWrapper key={label}>
+              <Number> {number}</Number>
+              <Label >{label}</Label>
+            </FlexWrapper>
+          )
+        })}
+
+      </AchievementsWrapper>
+    </AchievementsSectionBlack >
+  )
+}
+export default AchievementsProject
