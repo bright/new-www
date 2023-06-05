ee73fe93f398d69cb69f91a7151dcf14868499ce
CodersBros <p.grden@codersbros.pl>
2023-06-05 14:35:56 +0200

    hot fix format cms editor component

--- src/title-with-icon.cms.tsx
+++ src/title-with-icon.cms.tsx
@@ -1,46 +1,46 @@
-export const TitleWithIconCmsEditorComponent = {
-    id: 'TitleWithIcon',
-    fields: [
-        {
-            name: 'sectionTitle',
-            label: 'Section Title',
-            widget: 'string',
-        },
-        {
-            name: 'titleIconAlt',
-            label: 'Title Icon Alt',
-            widget: 'string',
-        },
-        {
-            name: 'titleIcon',
-            label: 'Title Icon',
-            widget: 'image',
-        },
-    ],
-    label: 'Title',
-    pattern: /^\s*<TitleWithIcon\s+sectionTitle=['"](.*?)['"]\s+titleIcon=['"](.*?)['"]\s+titleIconAlt=['"](.*?)['"]\s*\/>\s*$/,
-
-    fromBlock(match: RegExpMatchArray) {
-        return {
-            sectionTitle: match[1],
-            titleIcon: match[2],
-            titleIconAlt: match[3],
-
-        }
+import { EditorComponentOptions } from 'netlify-cms-core'
+export const TitleWithIconCmsEditorComponent: EditorComponentOptions = {
+  id: 'TitleWithIcon',
+  fields: [
+    {
+      name: 'sectionTitle',
+      label: 'Section Title',
+      widget: 'string',
     },
-    toBlock(props: {
-        sectionTitle: string
-        titleIcon: string
-        titleIconAlt: string
+    {
+      name: 'titleIconAlt',
+      label: 'Title Icon Alt',
+      widget: 'string',
+    },
+    {
+      name: 'titleIcon',
+      label: 'Title Icon',
+      widget: 'image',
+    },
+  ],
+  label: 'Title',
 
-    }) {
-        return `<TitleWithIcon sectionTitle='${props.sectionTitle}' titleIcon='${props.titleIcon}' titleIconAlt='${props.titleIconAlt}' />`
-    },
-    toPreview(props: {
-        sectionTitle: string
-        titleIcon: string
-        titleIconAlt: string
-    }) {
-        return this.toBlock(props)
-    },
+  pattern: /^\s*<TitleWithIcon\s+sectionTitle=['"](.*?)['"]\s+titleIcon=['"](.*?)['"]\s+titleIconAlt=['"](.*?)['"]\s*\/>\s*$/,
+
+  fromBlock(match: RegExpMatchArray) {
+    return {
+      sectionTitle: match[1],
+      titleIcon: match[2],
+      titleIconAlt: match[3],  
+    }
+  },
+  toBlock(props: {
+    sectionTitle: string
+    titleIcon: string
+    titleIconAlt: string  
+  }) {
+    return `<TitleWithIcon sectionTitle='${props.sectionTitle}' titleIcon='${props.titleIcon}' titleIconAlt='${props.titleIconAlt}' />`
+  },
+  toPreview(props: {
+    sectionTitle: string
+    titleIcon: string
+    titleIconAlt: string
+  }) {
+    return this.toBlock(props)
+  },
 }
