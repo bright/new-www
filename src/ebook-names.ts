cdc3cdbffa06391c272479aaffc9c94f60fd0f19
Piotr Mionskowski <piotr.mionskowski@brightinventions.pl>
2023-06-30 08:06:43 +0200

    Add From-MVP-to-a-Final-Product.pdf to a final product

--- src/ebook-names.ts
+++ src/ebook-names.ts
@@ -1,1 +1,4 @@
-export const ebookNames = ['25-Tools-And-Extra-Tactics-For-App-Security-Ebook.pdf']
+export const ebookNames = [
+  '25-Tools-And-Extra-Tactics-For-App-Security-Ebook.pdf',
+  'From-MVP-to-a-Final-Product.pdf'
+]
