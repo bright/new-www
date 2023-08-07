d9acb2c5947cb98cd7d7456db5d31d6439bb843e
Piotr Mionskowski <piotr.mionskowski@brightinventions.pl>
2023-08-07 13:18:16 +0200

    Pass ex to AuthorsPreview

--- src/cms/AuthorsPreview.tsx
+++ src/cms/AuthorsPreview.tsx
@@ -7,6 +7,7 @@ export function AuthorsPreview(props: AuthorDataProps) {
       slug={props.authorId}
       name={props.authorId ?? 'Name'}
       avatar={null as any}
+      ex={false}
       bio={'Bio'}
     />
   )
