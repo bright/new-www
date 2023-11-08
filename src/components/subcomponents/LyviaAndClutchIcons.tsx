8ec5529d81c4e2dd2cce3791c60463a0e04f68a4
CodersBros <p.grden@codersbros.pl>
2023-11-08 09:54:51 +0100

    change layout logo Lyvia and Clutch

--- src/components/subcomponents/LyviaAndClutchIcons.tsx
+++ src/components/subcomponents/LyviaAndClutchIcons.tsx
@@ -36,7 +36,7 @@ height: 100%;
 const LyviaAndClutchIcons = () => {
     return (
       <CustomSection paddingProps='35px 0 0 12px ' paddingLaptop='35px 0 0 12px' paddingTabletXL='35px 0 0 12px' paddingTablet='30px 0 0 12px' paddingMobileProps='30px 0 0 12px' >
-        <FlexWrapper desktopGap='30px' desktopDirection='column' tabletDirection='row'>
+        <FlexWrapper desktopGap='30px' desktopDirection='column' >
           <IconLink href='https://www.lyviagroup.com'
             target='_blank'
             rel='noopener noreferrer nofollow'>
