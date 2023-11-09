49caa9f5f8626297e2ee7360ce0f286f5705df0b
Piotr Mionskowski <piotr.mionskowski@brightinventions.pl>
2023-11-09 11:54:47 +0100

    Disable newsletter signup on blog pages

--- src/components/subcomponents/NewsletterWrapper.tsx
+++ src/components/subcomponents/NewsletterWrapper.tsx
@@ -3,20 +3,20 @@ import Newsletter from './Newsletter'
 
 export default function NewsletterWrapper() {
   const [isScrolledDown, setIsScrolledDown] = useState(false)
-
-  useEffect(() => {
-    const scrollListener = () => {
-      if (window.scrollY > 0 && !isScrolledDown) {
-        setIsScrolledDown(true)
-      } else {
-        setIsScrolledDown(false)
-      }
-    }
-    document.addEventListener('scroll', scrollListener)
-    return () => {
-      document.removeEventListener('scroll', scrollListener)
-    }
-  }, [])
+  // TODO: disable the newsletter until we have a new version
+  // useEffect(() => {
+  //   const scrollListener = () => {
+  //     if (window.scrollY > 0 && !isScrolledDown) {
+  //       setIsScrolledDown(true)
+  //     } else {
+  //       setIsScrolledDown(false)
+  //     }
+  //   }
+  //   document.addEventListener('scroll', scrollListener)
+  //   return () => {
+  //     document.removeEventListener('scroll', scrollListener)
+  //   }
+  // }, [])
 
   return <>{isScrolledDown && <Newsletter />}</>
 }
