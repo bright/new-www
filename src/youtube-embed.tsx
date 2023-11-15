28d4032e66ecc317d3e5a6af26ec8ce8bd369a09
CodersBros <p.grden@codersbros.pl>
2023-11-15 14:37:22 +0100

    add styles to YT embed globally

--- src/youtube-embed.tsx
+++ src/youtube-embed.tsx
@@ -2,10 +2,10 @@ import { YouTubeEmbed, YouTubeEmbedProps } from 'react-social-media-embed/dist/c
 import React from 'react'
 
 export const BrightYouTubeEmbed = (props: YouTubeEmbedProps) => {
-  const youTubeProps = { style: { maxWidth: "640px", display: "block", margin: "auto" } }
+  const youTubeProps = { style: { aspectRatio: '16/9' } }
   props = {
     width: '100%',
-    height: '360px',
+    height: '100%',
     youTubeProps,
     ...props
   }
