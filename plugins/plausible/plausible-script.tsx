610c1d5ef161289b4e4b442ce262b3f07dbb70a5
Michal Janiszewski <michal.janiszewski@brightinventions.pl>
2024-01-23 18:29:48 +0100

    Adds track custom event for Plausible

--- plugins/plausible/plausible-script.tsx
+++ plugins/plausible/plausible-script.tsx
@@ -1,9 +1,14 @@
-import { Script } from 'gatsby'
+import { Script, ScriptStrategy } from 'gatsby'
 import { siteUrl } from '../../src/site-metadata'
 import React from 'react'
 
 export function PlausibleScript() {
   return (
-    <Script src={'https://plausible.brightinventions.pl/js/script.js'} strategy='idle' data-domain={siteUrl.hostname} />
+    <>
+      <Script src={'https://plausible.brightinventions.pl/js/script.js'} strategy={ScriptStrategy.idle}
+              data-domain={siteUrl.hostname} />
+      <Script src={'https://plausible.brightinventions.pl/js/script.tagged-events.js'} strategy={ScriptStrategy.idle}
+              data-domain={siteUrl.hostname} />
+    </>
   )
 }
