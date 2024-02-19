195aea6f6e42fc7059c10534ffb2d9cd38582590
Izabela <82568642+izabelapawlik@users.noreply.github.com>
2024-02-19 19:03:39 +0100

    Update routing.ts

--- src/config/routing.ts
+++ src/config/routing.ts
@@ -48,4 +48,5 @@ export const routeLinks = {
          blockchainDevelopment: '/our-areas/blockchain/',
          bluetoothDevelopment: '/our-areas/bluetooth-development/',
          mvpdDevelopment: '/our-areas/mvp-development/',
+         healthcareDevelopment: 'healthcare-software-development/',
        }
