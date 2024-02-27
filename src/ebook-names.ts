a002eb5ffc7e85206f5d0de01af19194ad111f6b
Piotr Mionskowski <piotr.mionskowski@brightinventions.pl>
2024-02-27 10:59:33 +0100

    Add Scale-Your-Healthtech-Solution-Successfully ebook

--- src/ebook-names.ts
+++ src/ebook-names.ts
@@ -2,4 +2,5 @@ export const ebookNames = [
   '25-Tools-And-Extra-Tactics-For-App-Security-Ebook.pdf',
   'From-MVP-to-a-Final-Product.pdf',
   'Optimise-Sign-In-Experience.pdf',
+  'Scale-Your-Healthtech-Solution-Successfully.pdf',
 ]
