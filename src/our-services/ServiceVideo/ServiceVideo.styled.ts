2a091eb9f8279c550e476640de5764b4812310ef
RafalOpackiBrightInventions <rafal.opacki@brightinventions.pl>
2024-10-09 11:25:46 +0200

    cleanup

--- src/our-services/ServiceVideo/ServiceVideo.styled.ts
+++ src/our-services/ServiceVideo/ServiceVideo.styled.ts
@@ -1,11 +1,10 @@
-import { GatsbyImage } from 'gatsby-plugin-image'
 import styled from 'styled-components'
 
 export const VideoWrapper = styled.div`
   position: relative;
   width: 100%;
   max-width: 640px;
-  height: 360px;
+  max-height: 360px;
   overflow: hidden;
   border-radius: 16px;
 
@@ -14,13 +13,4 @@ export const VideoWrapper = styled.div`
     margin: auto;
     max-width: 100%;
   }
-`
-
-export const PlaceholderImage = styled(GatsbyImage)`
-  position: absolute !important;
-  top: 0;
-  right: 0;
-  z-index: 2;
-  height: 100%;
-  width: 100%;
 `
--- src/our-services/ServiceVideo/ServiceVideo.tsx
+++ src/our-services/ServiceVideo/ServiceVideo.tsx
@@ -1,61 +1,16 @@
-import { GatsbyImage } from 'gatsby-plugin-image'
-import styled from 'styled-components'
-import { IGatsbyImageData } from 'gatsby-plugin-image'
-import React, { useMemo, useState } from 'react'
-import { YouTubeEmbed } from 'react-social-media-embed'
-import { PlaceholderImage, VideoWrapper } from './ServiceVideo.styled'
+import React from 'react'
+import { VideoWrapper } from './ServiceVideo.styled'
 
 type ServiceVideoProps = {
-  videoUrl: string
-  videoPlaceholderImage?: IGatsbyImageData | undefined
-  videoFile?: string
+  videoFile: string
 }
 
-export const ServiceVideo: React.FC<ServiceVideoProps> = ({ videoUrl, videoPlaceholderImage, videoFile }) => {
-  const [shouldDisplayVideoPlaceholder, setShouldDisplayVideoPlaceholder] = useState(true)
-  const handleVideoStart = (): void => {
-    setShouldDisplayVideoPlaceholder(false)
-  }
-
-  const videoId: string | undefined = useMemo(() => {
-    // example url: 'https://www.youtube.com/watch?v=abcdefghijk';
-    // id: "abcdefghijk"
-    const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=)|youtu\.be\/)([^&?\/\s]{11})/
-    const match = videoUrl.match(regex)
-    return match ? match[1] : undefined
-  }, [videoUrl])
-
-  return videoFile ? (
-    <video autoPlay loop muted controls>
-      <source src={'/images/prompt_pos_integration_story_new.mp4'} type='video/mp4' />
-    </video>
-  ) : (
+export const ServiceVideo: React.FC<ServiceVideoProps> = ({ videoFile }) => {
+  return (
     <VideoWrapper>
-      {shouldDisplayVideoPlaceholder && <PlaceholderImage image={videoPlaceholderImage} alt='' />}
-
-      <YouTubeEmbed
-        placeholderDisabled
-        url={videoUrl}
-        youTubeProps={{
-          onPlay: handleVideoStart,
-          opts: {
-            playerVars: {
-              rel: 0, // Disable related videos after playback
-              autoplay: 1, // Autoplay the video
-              controls: 0, // Hide player controls
-              disablekb: 1, // Disable keyboard controls
-              fs: 0, // Disable full-screen button
-              iv_load_policy: 3, // Disable video annotations
-              // @ts-ignore
-              mute: 1, // Start video muted
-              loop: 1, // Loop video
-              playsinline: 1, // Play inline on mobile
-              playlist: videoId, // To allows loop
-            },
-          },
-          iframeClassName: 'iframe',
-        }}
-      />
+      <video width='100%' height='100%' autoPlay loop muted controls>
+        <source src={videoFile} type='video/mp4' />
+      </video>
     </VideoWrapper>
   )
 }
