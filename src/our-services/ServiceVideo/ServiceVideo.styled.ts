51a8cbc3554c21dab63780dab4fcd9041026d327
RafalOpackiBrightInventions <rafal.opacki@brightinventions.pl>
2024-10-10 09:29:16 +0200

    smol UI improvements

--- src/our-services/ServiceVideo/ServiceVideo.styled.ts
+++ src/our-services/ServiceVideo/ServiceVideo.styled.ts
@@ -9,9 +9,7 @@ export const VideoWrapper = styled.div`
 
   video {
     width: 100%;
-    height: 100%;
-    max-width: 640px;
-    max-height: 360px;
+    max-width: 960px;
   }
 
   @media ${variables.deviceWidthMin.tablet} {
