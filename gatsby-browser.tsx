e9247c147bacb249501b66b3131376b75da5fb9d
Krystian Kaminski <krystian.kaminski@brightinventions.pl>
2024-12-06 14:58:34 +0100

    Lead Forensics

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -14,9 +14,8 @@ import { loadConsentDecision } from './src/analytics/local-storage-constants'
 import { setupGtagTrackingConsent } from './plugins/google-gtag/tracking-consent'
 import { setupTrackingConsentInHotjar } from './plugins/hotjar/tracking-consent'
 import { setupTrackingConsentInLinkedIn } from './plugins/linkedin-pixel/tracking-consent'
+import { setupTrackingConsentInLeadForensics } from './plugins/lead-forensics/tracking-consent'
 import { CookieConsentContextWrapper } from './src/analytics/contextual-cookie-consent'
-import i18n from 'i18next'
-import { useSSR } from 'react-i18next'
 import { i18nForPageContext, i18nResources, withI18next } from './src/i18n'
 
 let nextRoute = ''
@@ -64,6 +63,10 @@ export const onClientEntry: GatsbyBrowser['onClientEntry'] = () => {
   }).catch(console.error)
 
   setupTrackingConsentInLinkedIn({
+    consentDecisionLoader: loadConsentDecision,
+  }).catch(console.error)
+
+  setupTrackingConsentInLeadForensics({
     consentDecisionLoader: loadConsentDecision,
   }).catch(console.error)
 }
