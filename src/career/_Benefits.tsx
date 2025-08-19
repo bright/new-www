22125b37b8ac3d833ab16128b9c8bee4b061bfbb
Izabela Pawlik <82568642+izabelapawlik@users.noreply.github.com>
2025-08-19 13:59:47 +0200

    Update career_Benefits.tsx

--- src/career/_Benefits.tsx
+++ src/career/_Benefits.tsx
@@ -191,16 +191,14 @@ const Benefits: React.FC<BenefitsProps> = ({ faqSlug }) => {
         title: 'Swimming workouts with pro triathlete',
         alt: 'Swimming workouts',
       },
-      { image: '/images/career/benefits/benefits_yoga.png', title: 'Friday yoga', alt: 'Friday yoga' },
+      { image: '/images/career/benefits/image11.png', title: 'Team retreats', alt: 'Team retreats' },
       { image: '/images/career/benefits/pets_friendly_office.png', title: 'Pet-friendly office', alt: 'Pet-friendly office' },
       { image: '/images/career/benefits/image5.png', title: 'English classes', alt: 'English classes' },
       { image: '/images/career/benefits/knowledge_sharing.png', title: 'Knowledge sharing initiatives', alt: 'Knowledge sharing initiatives' },
       ...(expanded
         ? [
-            { image: '/images/career/benefits/image11.png', title: 'Team retreats', alt: 'Team retreats' },
             { image: '/images/career/benefits/image8.png', title: 'Internal workshops', alt: 'Internal workshops' },
             { image: '/images/career/benefits/image9.png', title: 'Mentoring', alt: 'Mentoring' },
-            { image: '/images/career/benefits/image12.png', title: 'Board game Fridays', alt: 'Board game Fridays' },
           ]
         : []),
     ],
