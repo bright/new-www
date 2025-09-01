da43f7a259132d909b4a834576154d6e3aece03c
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2025-09-01 13:42:44 +0200

    Lazy load `Newsletter` component using `react-intersection-observer` to improve performance

--- src/components/subcomponents/NewsletterWrapper.tsx
+++ src/components/subcomponents/NewsletterWrapper.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Newsletter from './Newsletter'
 import { InView } from 'react-intersection-observer'
 
 export default function NewsletterWrapper() {
-  return <InView><Newsletter /></InView>
+  return <InView>
+    {({ inView, ref }) => (<div ref={ref}>({inView && <Newsletter />})</div>)}
+  </InView>
 }
